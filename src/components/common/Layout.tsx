27c4eb33546bd973655ac7d5149a2da4af89a3f1
Kled <1017yu@daum.net>
2023-09-04 18:20:42 +0900

    feat: 전역 스타일링 Layout 추가

---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,19 @@
+import { css } from '@emotion/react';
+import pretendard from '@/styles/font';
+
+function Layout({ children }: React.PropsWithChildren) {
+  return (
+    <div className={pretendard.className} css={layoutStyles}>
+      {children}
+    </div>
+  );
+}
+
+const layoutStyles = css`
+  padding: 0 16px;
+  margin: 0 auto;
+  min-width: 280px;
+  max-width: 1024px;
+`;
+
+export default Layout;
