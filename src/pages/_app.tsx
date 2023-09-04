cccb67421f9bb93ebc847690df90b29fc327bfd3
Kled <1017yu@daum.net>
2023-09-04 18:20:57 +0900

    feat: theme 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,17 @@
+import '@/styles/globals.css';
+import theme from '@/styles/theme';
+import Layout from '@/components/common/Layout';
+import { ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 }
+
+export default App;
