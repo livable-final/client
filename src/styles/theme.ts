ad129045f7055211df19f08f633242ad7f7c895d
Kled Yu <83483378+1017yu@users.noreply.github.com>
2023-09-04 22:07:48 +0900

    feat: 글로벌 스타일링, theme 세팅 등 기초 프로젝트 세팅 (#2)
    * asset: icons 내 svg파일 추가
    
    * chore: emotion config 설정
    
    * chore: eslint 변수 정의 설정
    
    * asset: Pretendard 폰트 세팅
    
    * feat:  next local font 설정
    
    * fix: 함수 선언 방식 컨벤션화
    
    * feat: color props 글로벌 버튼 추가
    
    * feat: 전역 스타일링 Layout 추가
    
    * feat: theme 설정
    
    * feat: global css 세팅
    
    * feat: 버튼 예시 추가
    
    * feat: secondary 버튼 추가
    
    * fix: title name 수정

---
+++ src/styles/theme.ts
@@ -0,0 +1,41 @@
+const theme = {
+  palette: {
+    title: '#111827',
+    white: '#FFFFFF',
+    blue: {
+      blue10: '#E7EEFF',
+      blue20: '#CAD9FA',
+      blue30: '#A6BEF4',
+      blue40: '#7EA2F3',
+      blue50: '#5085FB',
+      blue60: '#2563EB',
+      blue70: '#1A53D1',
+      blue80: '#1648B6',
+      blue90: '#163986',
+    },
+    greyscale: {
+      grey5: '#F9FAFB',
+      grey10: '#F0F1F3',
+      grey20: '#D7DCE3',
+      grey30: '#C3C6CC',
+      grey40: '#96A2AC',
+      grey50: '#747C84',
+      grey60: '#4A5662',
+      grey70: '#33404E',
+      grey80: '#243545',
+      grey90: '#162637',
+    },
+    state: {
+      danger: '#F13D2C',
+      warning: '#F78C35',
+    },
+    input: {
+      unabled: '#96A2AC',
+      default: '#4A5662',
+      enabled: '#162637',
+    },
+    // button: {},
+  },
+};
+
+export default theme;
