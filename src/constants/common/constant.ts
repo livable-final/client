55f1dcc58b15f0f266d2f837fd5d5dda7c2d7744
junehee <dev.junehee@gmail.com>
2023-09-05 23:35:38 +0900

    feat: 아이콘 이름 상수 추가

--- src/constants/common/constant.ts
+++ src/constants/common/constant.ts
@@ -1,11 +1,13 @@
 import theme from '@/styles/theme';
 import { ButtonColorsProps } from '@/types/common/button';
+import { CategoryColorsProps } from '@/types/common/category';
 
-const COMMON_BUTTON_COLORS: ButtonColorsProps = {
+// 공통 버튼
+export const COMMON_BUTTON_COLORS: ButtonColorsProps = {
   blue: {
-    background: `${theme.palette.blue.blue60}`,
+    background: `${theme.palette.bluescale.blue60}`,
     color: `${theme.palette.white}`,
-    isDisabled: `${theme.palette.blue.blue20}`,
+    isDisabled: `${theme.palette.bluescale.blue20}`,
   },
   grey: {
     background: `${theme.palette.greyscale.grey30}`,
@@ -14,7 +16,7 @@ const COMMON_BUTTON_COLORS: ButtonColorsProps = {
   },
   secondaryBlue: {
     background: `${theme.palette.white}`,
-    color: `${theme.palette.blue.blue60}`,
+    color: `${theme.palette.bluescale.blue60}`,
   },
   secondaryGrey: {
     background: `${theme.palette.white}`,
@@ -22,4 +24,31 @@ const COMMON_BUTTON_COLORS: ButtonColorsProps = {
   },
 };
 
-export default COMMON_BUTTON_COLORS;
+// 공통 카테고리
+export const COMMON_CATEGORY_COLORS: CategoryColorsProps = {
+  blue: {
+    border: `2px solid ${theme.palette.primary}`,
+    backgroundColor: `${theme.palette.white}`,
+    color: `${theme.palette.primary}`,
+  },
+  grey: {
+    border: `2px solid ${theme.palette.greyscale.grey20}`,
+    backgroundColor: `${theme.palette.white}`,
+    color: `${theme.palette.greyscale.grey40}`,
+  },
+};
+
+// 아이콘
+export const COMMON_ICON_NAMES = {
+  invitation: {
+    meeting: 'meeting',
+    interview: 'interview',
+    fixedTermWork: 'fixedTermWork',
+    seminar: 'seminar',
+    as: 'as',
+    etc: 'etc',
+  },
+  home: {
+    // 추후 보강 예정
+  },
+};
