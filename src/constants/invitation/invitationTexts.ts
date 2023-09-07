bd0f002a982940040d7b879184167670b5d9e6c5
junehee <dev.junehee@gmail.com>
2023-09-08 00:51:09 +0900

    feat: 방문자 초대장 텍스트 상수 추가

---
+++ src/constants/invitation/invitationTexts.ts
@@ -0,0 +1,16 @@
+const INVITATION_TEXTS = {
+  purpose: '방문증을 만드는 이유는 무엇인가요?',
+  description: {
+    meeting:
+      '예약한 회의실을 선택하거나 회의의 장소·날짜·시간을 자유롭게 선택할 수 있어요',
+    interview: '면접의 장소·날짜·시간을 입력해 한 명에게 보낼 수 있어요',
+    fixedTermWork: '장소·기간(시작일~종료일)·시간을 자유롭게 선택할 수 있어요',
+    seminar: '장소·기간(시작일~종료일)·시간을 자유롭게 선택할 수 있어요',
+    as: '장소·기간(시작일~종료일)·시간을 자유롭게 선택할 수 있어요',
+    etc: '장소·기간(시작일~종료일)·시간을 자유롭게 선택할 수 있어요',
+  },
+  next: '다음',
+  inputPlaceholder: '방문목적을 직접 입력해 주세요.',
+};
+
+export default INVITATION_TEXTS;
