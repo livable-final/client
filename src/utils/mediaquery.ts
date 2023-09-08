e7bc326459a393cb1a560be2af9b6920bd73da6a
junehee <dev.junehee@gmail.com>
2023-09-08 19:55:31 +0900

    refactor: 미디어쿼리 breakpoints 변경

--- src/utils/mediaquery.ts
+++ src/utils/mediaquery.ts
@@ -1,10 +1,10 @@
-const breakpoints = [360, 480, 640];
+const breakpoints = [361, 481, 641];
 
-// 기본 사이즈 : small view (280 ~ 359)
+// 기본 사이즈 : small view (280 ~ 360)
 const mq = {
-  md: `@media (min-width: ${breakpoints[0]}px)`, // 360 ~ 479
-  lg: `@media (min-width: ${breakpoints[1]}px)`, // 480 ~ 639
-  tab: `@media (min-width: ${breakpoints[2]}px)`, // 640 ~ 1024
+  md: `@media (min-width: ${breakpoints[0]}px)`, // 361 ~ 480
+  lg: `@media (min-width: ${breakpoints[1]}px)`, // 481 ~ 640
+  tab: `@media (min-width: ${breakpoints[2]}px)`, // 641 ~ 1024
 };
 
 export default mq;
