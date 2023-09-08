a907e9426d0979c25ab675aa849795e88df33708
junehee <dev.junehee@gmail.com>
2023-09-08 17:57:04 +0900

    feat: 미디어쿼리 반응형 모듈 함수

--- src/utils/mediaquery.ts
+++ src/utils/mediaquery.ts
@@ -1,10 +1,10 @@
-const breakpoints = [280, 360, 480, 670];
+const breakpoints = [360, 480, 640];
 
+// 기본 사이즈 : small view (280 ~ 359)
 const mq = {
-  sm: `@media (max-width: ${breakpoints[0]}px)`,
-  md: `@media (max-width: ${breakpoints[1]}px)`,
-  lg: `@media (max-width: ${breakpoints[2]}px)`,
-  tab: `@media (max-width: ${breakpoints[3]}px)`,
+  md: `@media (min-width: ${breakpoints[0]}px)`, // 360 ~ 479
+  lg: `@media (min-width: ${breakpoints[1]}px)`, // 480 ~ 639
+  tab: `@media (min-width: ${breakpoints[2]}px)`, // 640 ~ 1024
 };
 
 export default mq;
