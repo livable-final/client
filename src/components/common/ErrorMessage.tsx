fbe194bc6b54057ac7eabf19ace43cbe908a82da
Siwoo Lee <siwoo28@kakao.com>
2023-09-11 11:01:53 +0900

    feat: errorMessage type 미지정시 출력값 추가

--- src/components/common/ErrorMessage.tsx
+++ src/components/common/ErrorMessage.tsx
@@ -6,13 +6,20 @@ import { ErrorIcon } from '@/assets/icons';
 import { ErrorMessageProps } from '@/types/common/errorMessage';
 
 function ErrorMessage({ errorType }: ErrorMessageProps) {
-  if (errorType === 'test')
-    return (
-      <div css={errorContainerStyles}>
-        <ErrorIcon color={theme.palette.state.warning} />
-        <div css={errorTextStyles}>{COMMON_ERROR_MESSAGE.test}</div>
-      </div>
-    );
+  const errorMessage = (error: string | undefined) => {
+    if (error === 'test')
+      return <div css={errorTextStyles}>{COMMON_ERROR_MESSAGE.test}</div>;
+    if (error === 'test2')
+      return <div css={errorTextStyles}>{COMMON_ERROR_MESSAGE.test2}</div>;
+    return <div css={errorTextStyles}>errorType지정 안했을경우 에러메세지</div>;
+  };
+
+  return (
+    <div css={errorContainerStyles}>
+      <ErrorIcon color={theme.palette.state.warning} />
+      {errorMessage(errorType)}
+    </div>
+  );
 }
 
 const errorContainerStyles = () => css`
