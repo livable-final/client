eef169393004288967ae0292b68faf083165637e
junehee <dev.junehee@gmail.com>
2023-09-12 01:21:26 +0900

    test: 컴포넌트 확인용 테스트 코드 작성

--- src/pages/invitation/create.tsx
+++ src/pages/invitation/create.tsx
@@ -1,5 +1,15 @@
+import InvitationPurpose from '@/components/invitation/create/InvitationPurpose';
+import InvitationInfo from '@/components/invitation/create/InvitationInfo';
+import InvitationDate from '@/components/invitation/create/InvitationDate';
+import Header from '@/components/common/Header';
+
 function Create() {
-  return <div>Create</div>;
+  return (
+    <>
+      {/* <Header /> */}
+      <InvitationDate />
+    </>
+  );
 }
 
 export default Create;
