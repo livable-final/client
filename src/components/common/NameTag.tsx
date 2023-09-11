cb909a7e5598e9eedc74d56268a71bffceee90c4
junehee <dev.junehee@gmail.com>
2023-09-12 01:14:52 +0900

    feat: 네임태그 컴포넌트 구현

---
+++ src/components/common/NameTag.tsx
@@ -0,0 +1,42 @@
+import theme from '@/styles/theme';
+import { ExitSmall } from '@/assets/icons';
+import { css } from '@emotion/react';
+import { NameTagProps } from '@/types/common/nameTag';
+
+function NameTag({ name, onClick }: NameTagProps) {
+  return (
+    <button type="button" css={nameTagContainerStyles(name)}>
+      <div css={textStyles}>{name}</div>
+      <button type="button" css={iconStyles} onClick={onClick}>
+        <ExitSmall />
+      </button>
+    </button>
+  );
+}
+
+const nameTagContainerStyles = (name: string) => css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  width: ${name.length < 3 ? `90px` : name.length > 3 ? `116px` : `103px`};
+  height: 42px;
+  border-radius: 100px;
+  background-color: ${theme.palette.greyscale.grey5};
+`;
+
+const textStyles = css`
+  color: ${theme.palette.greyscale.grey50};
+  font: ${theme.font.body.body2_400};
+  line-height: 22px;
+`;
+
+const iconStyles = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 2px;
+  cursor: pointer;
+`;
+
+export default NameTag;
