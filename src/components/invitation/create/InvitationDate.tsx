3a4e94fa5e19be9b660b0c46b5b9f932cda09212
junehee <dev.junehee@gmail.com>
2023-09-12 20:29:48 +0900

    rename: 파일명 변경

--- src/components/invitation/create/InvitationDate.tsx
+++ src/components/invitation/create/InvitationDate.tsx
@@ -1,63 +1,94 @@
 import Input from '@/components/common/Input';
-import NameTag from '@/components/common/NameTag';
-import Add from '@/components/common/Add';
-import Button from '@/components/common/Button';
 import { css } from '@emotion/react';
+import { Location, Calendar, Clock } from '@/assets/icons';
+import theme from '@/styles/theme';
 
 function InvitationDate() {
-  const nameTagTest = [
-    '고애신',
-    '유진초이',
-    '김희성',
-    '쿠도히나',
-    '구동매',
-    '임관수',
-    '카일',
-    '도미',
-  ];
-
   return (
-    <div css={dateContainerStyles}>
+    <div>
+      <div>초대 정보</div>
       <div>
-        <div>초대 정보</div>
-        <div>
-          <Input
-            variant="default"
-            textarea
-            placeholder="방문하시는 분이 장소를 쉽게 찾을 수 있게 팁을 알려주세요."
-          />
+        {/* 장소 선택 */}
+        <div css={placeInputStyles}>
+          <div className="icon">
+            <Location />
+          </div>
+          <input css={inputStyles} type="text" placeholder="장소 선택" />
         </div>
-      </div>
-      <div css={invitationListContainerStyles}>
-        초대 목록
-        <div css={invitationListStyles}>
-          <Add isBlue onClick={() => alert('인원 초대 테스트')} />
-          {nameTagTest.map((name) => (
-            <NameTag
-              key={name}
-              name={name}
-              onClick={() => alert('삭제 기능 추가 예정')}
-            />
-          ))}
+        {/* 날짜, 시간 선택 */}
+        <div css={dateTimeInputStyles}>
+          <div className="date">
+            <div className="icon">
+              <Calendar />
+            </div>
+            <input css={inputStyles} type="text" placeholder="날짜 선택" />
+          </div>
+          <div className="time">
+            <div className="icon">
+              <Clock />
+            </div>
+            <input css={inputStyles} type="text" placeholder="시간 선택" />
+          </div>
         </div>
+        {/* 꿀팁 작성 */}
+        <Input
+          variant="default"
+          textarea
+          placeholder="방문하시는 분이 장소를 쉽게 찾을 수 있게 팁을 알려주세요."
+          row={4}
+          maxLength={100}
+        />
       </div>
-      <Button content="초대장 보내기" variant="blue" />
     </div>
   );
 }
 
-const dateContainerStyles = css``;
-
-const invitationListContainerStyles = css`
+const placeInputStyles = css`
   display: flex;
-  flex-direction: column;
-  gap: 16px;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid ${theme.palette.greyscale.grey10};
+  border-radius: 12px;
+  padding: 0 8px 0 16px;
+
+  .icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 24px;
+    height: 24px;
+  }
 `;
 
-const invitationListStyles = css`
+const dateTimeInputStyles = css`
   display: flex;
-  flex-wrap: wrap;
-  gap: 9px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid ${theme.palette.greyscale.grey10};
+  border-radius: 12px;
+  padding: 0 8px 0 16px;
+
+  .date,
+  .time {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 12px;
+
+    .icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 24px;
+      height: 24px;
+    }
+  }
+`;
+
+const inputStyles = css`
+  border: none;
+  border-radius: 12px;
 `;
 
 export default InvitationDate;
--- src/components/invitation/create/InvitationPurposeContainer.tsx
+++ src/components/invitation/create/InvitationPurposeContainer.tsx
@@ -1,10 +1,8 @@
 import theme from '@/styles/theme';
-import Header from '@/components/common/Header';
 import Category from '@/components/common/Category';
-import Button from '@/components/common/Button';
 import Icons from '@/components/common/Icons';
 import CREATE_TEXTS from '@/constants/invitation/createTexts';
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 import { css } from '@emotion/react';
 import { COMMON_CATEGORIES } from '@/constants/common';
 import {
@@ -17,45 +15,15 @@ import Input from '@/components/common/Input';
 
 function InvitationPurpose() {
   const { invitation }: CategoryInvitation = COMMON_CATEGORIES;
-  const {
-    purpose,
-    header,
-    description,
-    button,
-    inputPlaceholder,
-  }: InvitationTexts = CREATE_TEXTS;
+  const { purpose, description, inputPlaceholder }: InvitationTexts =
+    CREATE_TEXTS;
 
   const categories: CommonCategory[] = Object.values(invitation);
 
   const [selectedCategory, setSelectedCategory] = useState<string>('meeting');
-  const [purposeText, setPurposeText] = useState<string>('');
-  const [btnDisplay, setBtnDisplay] = useState<string>('block');
-
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setPurposeText(e.target.value);
-  };
-
-  const onFocusHandler = () => {
-    setBtnDisplay('none');
-  };
-
-  const onBlurHandler = () => {
-    setTimeout(() => {
-      setBtnDisplay('block');
-    }, 250);
-  };
 
   return (
     <div css={purposeContainerStyles}>
-      <div css={headerWrapperStyles}>
-        {/* 방문자 초대장 완성 시 미리보기 클릭 이벤트 연결 필요 */}
-        <Header
-          title={header.default}
-          type="text"
-          text="미리보기"
-          onClick={() => alert('미리보기 테스트')}
-        />
-      </div>
       <div css={purposeQuestionStyles}>
         <div>{purpose}</div>
       </div>
@@ -84,20 +52,13 @@ function InvitationPurpose() {
           <div css={iconWrapperStyles}>
             <Icons icon="info" color="grey" />
           </div>
-          {description[selectedCategory]}
+          <div>{description[selectedCategory]}</div>
         </div>
         <div css={inputWrapperStyles}>
           {selectedCategory === 'etc' && (
             <Input variant="default" placeholder={inputPlaceholder} textarea />
           )}
         </div>
-      </div>
-      <div
-        css={buttonWrapperStyles(btnDisplay)}
-        onFocus={onFocusHandler}
-        onBlur={onBlurHandler}
-      >
-        <Button content={button.next} variant="blue" />
       </div>
     </div>
   );
@@ -122,49 +83,28 @@ const purposeContainerStyles = css`
   }
 `;
 
-const headerWrapperStyles = css`
-  min-width: 280px;
-  max-width: 360px;
-
-  ${mq.md} {
-    min-width: 361px;
-    max-width: 480px;
-  }
-  ${mq.lg} {
-    min-width: 481px;
-    max-width: 640px;
-  }
-  ${mq.tab} {
-    min-width: 641px;
-    max-width: 1024px;
-  }
-`;
-
 const purposeQuestionStyles = css`
   display: flex;
-  /* justify-content: flex-start;
-  align-items: flex-start; */
+  width: 100%;
   min-width: 280px;
   max-width: 360px;
   height: 28px;
   padding-left: 4px;
-  text-align: left;
   div {
     display: flex;
-    width: 100%;
     font: ${theme.font.title.title2_500};
     line-height: 28px;
     text-align: left;
   }
 
   ${mq.md} {
-    max-width: 480px;
+    max-width: 360px;
   }
   ${mq.lg} {
-    max-width: 640px;
+    max-width: 360px;
   }
   ${mq.tab} {
-    max-width: 1024px;
+    max-width: 420px;
   }
 `;
 
@@ -189,11 +129,9 @@ const categoryWrapperStyles = css`
   ${mq.md} {
     max-width: 360px;
   }
-
   ${mq.lg} {
     max-width: 360px;
   }
-
   ${mq.tab} {
     max-width: 480px;
   }
@@ -202,26 +140,27 @@ const categoryWrapperStyles = css`
 const descriptionWrapperStyles = css`
   display: flex;
   gap: 4px;
-  min-width: 280px;
-  max-width: 360px;
+  width: 100%;
+  min-width: 260px;
+  max-width: 280px;
   height: 44px;
   padding: 0 20px 0;
-  color: ${theme.palette.greyscale.grey50};
-  font: ${theme.font.body.body2_400};
-  line-height: 22px;
-  text-align: left;
+  div {
+    display: flex;
+    color: ${theme.palette.greyscale.grey50};
+    font: ${theme.font.body.body2_400};
+    line-height: 22px;
+    text-align: left;
+  }
 
   ${mq.md} {
-    min-width: 361px;
-    max-width: 480px;
+    max-width: 360px;
   }
   ${mq.lg} {
-    min-width: 481px;
-    max-width: 640px;
+    max-width: 360px;
   }
   ${mq.tab} {
-    min-width: 641px;
-    max-width: 1024px;
+    max-width: 440px;
   }
 `;
 
@@ -230,42 +169,16 @@ const iconWrapperStyles = css`
 `;
 
 const inputWrapperStyles = css`
-  /* border: 1px solid red; */
-  min-width: 280px;
-  max-width: 360px;
+  max-width: 280px;
 
   ${mq.md} {
-    min-width: 361px;
-    max-width: 480px;
+    min-width: 360px;
   }
   ${mq.lg} {
-    min-width: 481px;
-    max-width: 640px;
+    min-width: 400px;
   }
   ${mq.tab} {
-    min-width: 641px;
-    max-width: 1024px;
-  }
-`;
-
-const buttonWrapperStyles = (btnDisplay: string) => css`
-  display: ${btnDisplay};
-  position: fixed;
-  bottom: 0;
-  min-width: 280px;
-  max-width: 360px;
-  margin-bottom: 20px;
-
-  ${mq.md} {
-    min-width: 361px;
-    max-width: 480px;
-  }
-  ${mq.lg} {
-    min-width: 481px;
-    max-width: 640px;
-  }
-  ${mq.tab} {
-    min-width: 641px;
+    min-width: 400px;
     max-width: 1024px;
   }
 `;
