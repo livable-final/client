4203c0345b6807045ef4056f6c2f1ad3eb011171
Siwoo Lee <siwoo28@kakao.com>
2023-09-12 23:46:39 +0900

    design: invitation view 상세정보 컴포넌트 생성

---
+++ src/components/invitation/view/InvitationInfo.tsx
@@ -0,0 +1,9 @@
+import { css } from '@emotion/react';
+
+function InvitationInfo() {
+  return <div css={InfoContainerStyles}>안녕</div>;
+}
+const InfoContainerStyles = css`
+  width: 100%;
+`;
+export default InvitationInfo;
