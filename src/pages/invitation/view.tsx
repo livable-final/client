a0fb4f4aac979250f3ce0bb49f284df21b6493df
Siwoo Lee <siwoo28@kakao.com>
2023-09-13 21:40:57 +0900

    design: invitaion main view page 레이아웃

--- src/pages/invitation/view.tsx
+++ src/pages/invitation/view.tsx
@@ -1,10 +1,22 @@
 import ViewForm from '@/components/auth/ViewForm';
 import Header from '@/components/common/Header';
-import Layout from '@/components/common/Layout';
+import Button from '@/components/common/Button';
 import InvitationInfoContainer from '@/components/invitation/view/InvitationInfoContainer';
+import InvitationCarouselContainer from '@/components/invitation/view/InvitationCarouselContainer';
 
 function View() {
-  return <ViewForm />;
+  const onClickHandler = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <Header title="식스센스 방문증" onClick={onClickHandler} />
+      <InvitationInfoContainer />
+      {/* 그랑서울 근처 식당 카페 테이블 부분 */}
+      <InvitationCarouselContainer />
+      <Button content="관리실" variant="secondaryGrey" />
+    </div>
+  );
 }
 
 export default View;
