04d16169e46afee2fb78921a192034589f05c5dc
Siwoo Lee <siwoo28@kakao.com>
2023-09-15 18:21:54 +0900

    design: view 페이지와  초대자 정보 페이지 연결

--- src/components/Auth/ViewForm.tsx
+++ src/components/Auth/ViewForm.tsx
@@ -1,19 +1,31 @@
 import useViewStore from '@/stores/useViewStore';
-import InvitationOfficeInfo from '../invitation/view/InvitationOfficeInfo';
+import InvitationOfficeInfo from '@/components/invitation/view/InvitationOfficeInfo';
+import InvitationHostInfo from '@/components/invitation/view/InvitationHostInfo';
 
 function ViewForm() {
   const { nextComponents, setNextComponent } = useViewStore();
   if (nextComponents === 'InvitationOfficeInfo') {
     return <InvitationOfficeInfo />;
   }
+  if (nextComponents === 'InvitationHostInfo') {
+    return <InvitationHostInfo />;
+  }
 
-  const onClickHandler = () => {
-    setNextComponent('InvitationOfficeInfo');
+  const onClickHandler = (event: React.MouseEvent) => {
+    setNextComponent((event.target as HTMLInputElement).value);
   };
+  // 초대장 메인 페이지가 아랫단 시설 부분이 확정이 안나서 작업 보류중에 있습니다! 레이아웃 작업 끝나면 각 버튼에 연결해놓겠습니다!
   return (
     <div>
-      <button type="button" onClick={onClickHandler}>
+      <button
+        type="button"
+        value="InvitationOfficeInfo"
+        onClick={onClickHandler}
+      >
         OfficeInfo 페이지로 이동
+      </button>
+      <button type="button" value="InvitationHostInfo" onClick={onClickHandler}>
+        HostInfo 페이지로 이동
       </button>
     </div>
   );
