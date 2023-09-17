c192bc138ff766365d6f9085e4efee059cbe4d46
Kled <1017yu@daum.net>
2023-09-17 15:24:21 +0900

    feat: 룰렛 기능 생성 완료

--- src/components/Roulette.tsx
+++ src/components/Roulette.tsx
@@ -1,148 +1,132 @@
-import { useEffect, useState } from 'react';
-import { AnimatePresence, motion, Variants } from 'framer-motion';
-
-// 임시 더미데이터
-const response = [
-  {
-    categoryName: '한식',
-    menuNames: ['국밥', '뚝배기 불고기', '제육볶음', '백반', '짜글이'],
-  },
-  {
-    categoryName: '중식',
-    menuNames: ['짜장면', '짬뽕', '마라탕', '도삭면'],
-  },
-  {
-    categoryName: '양식',
-    menuNames: ['햄버거', '치킨', '피자', '샐러드', '스테이크'],
-  },
-  { categoryName: '일식', menuNames: ['돈까스', '텐동', '초밥', '라멘'] },
-  { categoryName: '분식', menuNames: ['떡볶이', '김밥', '쫄면', '땡초우동'] },
-  { categoryName: '아시안', menuNames: ['쌀국수', '분짜', '팟타이'] },
-];
-
-interface VariantProps {
-  scaleY: number;
-  y: string | number;
-  opacity: number;
-  filter?: string;
-}
+import { css } from '@emotion/react';
+import { useState } from 'react';
+import {
+  ROULETTE_CONSTANTS,
+  DUMMY_RESPONSE,
+} from '@/constants/lunch/rouletteTexts';
+import Button from '@/components/common/Button';
+import {
+  findRandomMenus,
+  selectRandomCategory,
+  selectRandomMenus,
+} from '@/utils/selectRandomItem';
+import theme from '@/styles/theme';
 
 function Roulette() {
-  const [category, setCategory] = useState('카테고리');
-  const [menu, setMenu] = useState('메뉴명');
-  const [count, setCount] = useState(0);
-  const [currentIdx, setCurrentIdx] = useState(0);
-  const [lastIdx, setLastIdx] = useState(0);
+  const [categoryState, setCategoryState] = useState('카테고리'); // 랜덤 카테고리명
+  const [menuState, setMenuState] = useState('메뉴'); // 랜덤 메뉴명
+  const [menuIdState, setMenuIdState] = useState(0);
   const [isChecked, setIsChecked] = useState(false);
+  const [isShown, setIsShown] = useState(false);
+  const { time, content } = ROULETTE_CONSTANTS;
 
-  // 카테고리 배열 string[]
-  const categories = Object.values(response).map((value) => value.categoryName);
-
-  // 카테고리별 메뉴 객체 string: string[]
-  const objectedMenus: Record<string, string[]> = {};
-  response.forEach((item) => {
-    objectedMenus[item.categoryName] = item.menuNames;
-  });
-
-  // 카테고리 불문 모든 메뉴 배열 string[]
-  const menus = Object.values(response)
-    .map((value) => value.menuNames)
-    .reduce((acc, cur) => acc.concat(cur));
-
-  useEffect(() => {
-    const interval = setInterval(
-      () => {
-        setCurrentIdx((prev) => {
-          return prev < menus.length - 1 ? prev + 1 : prev;
-        });
-      },
-      getDuration(10, currentIdx),
-    );
-
-    return () => clearInterval(interval);
-  }, [currentIdx, menus.length]);
-
-  const variants: Variants = {
-    initial: { scaleY: 0.3, y: '-50%', opacity: 0 },
-    animate: ({ isLast }) => {
-      const props: VariantProps = { scaleY: 1, y: 0, opacity: 1 };
-      if (!isLast) props.filter = 'blur(1.1px)';
-
-      return props;
-    },
-    exit: { scaleY: 0.3, y: '50%', opacity: 0 },
-  };
-
-  function getDuration(base: number, Idx: number) {
-    return base * (Idx + 1) * 0.5;
-  }
-
-  // length를 매개변수로 받아 length에 따른 랜덤 number 반환
-  const onRandomHandler = (length: number) => {
-    return Math.floor(Math.random() * length);
-  };
-
+  // 버튼 클릭 시 카테고리와 메뉴를 셔플하는 함수
   const onClickHandler = () => {
-    const categoryIdx = onRandomHandler(categories.length);
-    const menuIdx = onRandomHandler(categories[categoryIdx].length);
-    setLastIdx(menus.length - 1 - count);
-
+    setIsShown(false);
+    // 체크박스가 체크되었을 때
     if (isChecked) {
-      // 업데이트된 카테고리에 따른 랜덤 메뉴 state 업데이트
-      setMenu(objectedMenus[menu][menuIdx]);
+      // 메뉴만 랜덤하게 변경
+      const menuInterval = setInterval(() => {
+        const menus = findRandomMenus(DUMMY_RESPONSE, categoryState);
+        const { name, menuId } = menus; // 랜덤 메뉴의 ID
+        setMenuState(name);
+        setMenuIdState(menuId);
+
+        // 메뉴 셔플 속도 (100ms)
+      }, time.interval);
+
+      // 모든 선택이 완료되는 시간 인터벌 (500ms)
+      setTimeout(() => {
+        clearInterval(menuInterval);
+        setIsShown(true);
+      }, time.duration.fixed);
     } else {
-      // 랜덤하게 카테고리 state 업데이트
-      setCategory(categories[categoryIdx]);
+      // 체크박스가 체크되지 않았을 때
+      const categoryInterval = setInterval(() => {
+        const category = selectRandomCategory(DUMMY_RESPONSE);
+        const { categoryName } = category;
+        setCategoryState(categoryName);
 
-      // 업데이트된 카테고리에 따른 랜덤 메뉴 state 업데이트
-      setMenu(objectedMenus[categories[categoryIdx]][menuIdx]);
+        // 카테고리에 따른 메뉴 선택 인터벌 함수
+        const menuInterval = setInterval(() => {
+          const menus = selectRandomMenus(category);
+          const { name, menuId } = menus; // 랜덤 메뉴의 ID
+          setMenuState(name);
+          setMenuIdState(menuId);
+
+          // 메뉴 셔플 속도 (100ms)
+        }, time.interval);
+
+        // 카테고리 선택 완료 후 메뉴 선택까지의 시간 인터벌 (500ms)
+        setTimeout(() => {
+          clearInterval(menuInterval);
+        }, time.duration.menu);
+
+        // 카테고리 셔플 속도 (100ms)
+      }, time.interval);
+
+      // 카테고리 선택 완료까지의 시간 인터벌 (3500ms)
+      setTimeout(() => {
+        clearInterval(categoryInterval);
+      }, time.duration.category);
+
+      // 모든 선택이 완료되는 시간 인터벌 (500ms + 3500ms = 4000ms)
+      setTimeout(() => {
+        setIsShown(true);
+      }, time.duration.category + time.duration.menu);
     }
-    setCurrentIdx(0);
-    setCount((prev) => {
-      return prev < menus.length - 1 ? prev + 1 : 0;
-    });
   };
-
-  // const onCheckHandler = () => {
-  //   setIsChecked((prev) => !prev);
-  // };
 
   return (
-    <div css="flex justify-between">
-      <AnimatePresence mode="popLayout">
-        {menus.map((text, i) => {
-          const isLast = i === lastIdx;
-
-          return (
-            i === currentIdx && (
-              <motion.p
-                className="overflow-hidden text-7xl font-thin"
-                key={text}
-                custom={{ isLast }}
-                variants={variants}
-                initial="initial"
-                animate="animate"
-                exit="exit"
-                transition={{
-                  duration: getDuration(isLast ? 0.1 : 0.01, i),
-                  ease: isLast ? 'easeInOut' : 'linear',
-                }}
-              >
-                {text}
-              </motion.p>
-            )
-          );
-        })}
-      </AnimatePresence>
-      <motion.button
-        className="mr-[650px]"
+    <>
+      <div css={WrapperStyles}>
+        <span css={spanStyles}>{categoryState}</span>
+        <span css={spanStyles}>{menuState}</span>
+      </div>
+      <Button
         onClick={onClickHandler}
-        whileTap={{ scale: 0.9 }}
-      >
-        button
-      </motion.button>
-    </div>
+        content={content.button}
+        isDisabled={!isShown && menuIdState > 0}
+        variant="blue"
+      />
+      <div css={checkboxStyles(isShown)}>
+        <input
+          css={inputStyles}
+          type="checkbox"
+          checked={isChecked}
+          onChange={() => setIsChecked(!isChecked)}
+        />
+        <label>{`'${categoryState}' 고정`}</label>
+      </div>
+      {`이 음식의 ID값입니다 :> ${menuIdState}`}
+    </>
   );
 }
+
+const WrapperStyles = css`
+  display: flex;
+  width: 320px;
+  justify-content: space-around;
+  margin: auto;
+`;
+
+const spanStyles = css`
+  min-width: 100px;
+  text-align: center;
+  font: ${theme.font.body.body1_600};
+`;
+
+const checkboxStyles = (isShown: boolean) => css`
+  display: ${isShown ? 'flex' : 'none'};
+  justify-content: center;
+  gap: 16px;
+`;
+
+const inputStyles = css`
+  width: 16px;
+  height: auto;
+  min-width: 0;
+  max-width: 40px;
+`;
 
 export default Roulette;
