aa6fa1c4202b0fb67999d56798d91d2731afe8bf
Kled <1017yu@daum.net>
2023-09-19 00:28:22 +0900

    feat: 홈리디자인 파트 상수 생성

---
+++ src/constants/home/homeTexts.ts
@@ -0,0 +1,9 @@
+const HOME_TEXTS = {
+  gnb: {
+    home: { name: '홈', url: '/', icon: 'Home' },
+    lunch: { name: '오점완', url: '/lunch', icon: 'Home' },
+    user: { name: 'MY', url: '/user', icon: 'Home' },
+  },
+};
+
+export default HOME_TEXTS;
