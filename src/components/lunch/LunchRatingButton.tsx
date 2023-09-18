c24d7b4c3770e0565ac2ba3a14722bc18ce16bfb
ChoEun-Sang <wh94es@gmail.com>
2023-09-19 02:17:19 +0900

    feat: 맛있오, 맛없오 평가 버튼 구현

---
+++ src/components/lunch/LunchRatingButton.tsx
@@ -0,0 +1,69 @@
+import theme from '@/styles/theme';
+import { css } from '@emotion/react';
+import { CALENDAR_CONTENT } from '@/constants/lunch';
+import { Smile, Confused } from '@/assets/icons';
+import { LunchRatingButtonProps } from '@/types/lunch/calendar';
+
+function LunchRatingButton({
+  title,
+  isChecked,
+  onClick,
+}: LunchRatingButtonProps) {
+  const { button } = CALENDAR_CONTENT;
+
+  let icon;
+
+  switch (title) {
+    case button.button5.good:
+      icon = <Smile />;
+      break;
+    case button.button5.bad:
+      icon = <Confused />;
+      break;
+    default:
+      break;
+  }
+  return (
+    <button type="button" css={buttonStyles} onClick={onClick}>
+      <div css={iconStyles(isChecked)}>{icon}</div>
+      <p>{title}</p>
+    </button>
+  );
+}
+
+const buttonStyles = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  width: 100%;
+  padding: 16px 0;
+  border-radius: 16px;
+  border: 1px solid ${theme.palette.greyscale.grey10};
+  background-color: ${theme.palette.greyscale.grey5};
+  color: ${theme.palette.greyscale.grey50};
+  font: ${theme.font.body.body1_500};
+
+  &:active {
+    border: 2px solid ${theme.palette.orange};
+    background-color: ${theme.palette.white};
+    color: ${theme.palette.orange};
+
+    svg {
+      filter: none;
+      transform: scale(1.2);
+      transition: transform 0.2s ease;
+    }
+  }
+`;
+
+const iconStyles = (isChecked: boolean) => css`
+  margin-bottom: 4px;
+
+  svg {
+    filter: ${isChecked ? `saturate(5%)` : `none`};
+  }
+`;
+
+export default LunchRatingButton;
