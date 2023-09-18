57facf363e11b2cc38e003899597afea7d6a3158
ChoEun-Sang <wh94es@gmail.com>
2023-09-19 02:20:00 +0900

    feat: 리뷰 작성 - 외식/구내식당/도시락 리뷰 작성 페이지 구현

---
+++ src/components/lunch/calendar/LunchCalendarEatOut.tsx
@@ -0,0 +1,98 @@
+import { useState, useRef } from 'react';
+import { css } from '@emotion/react';
+import theme from '@/styles/theme';
+import Header from '@/components/common/Header';
+import { CALENDAR_CONTENT } from '@/constants/lunch';
+import Button from '@/components/common/Button';
+import Input from '@/components/common/Input';
+import LunchSubTitle from '@/components/lunch/LunchSubTitle';
+import LunchRatingButton from '@/components/lunch/LunchRatingButton';
+import LunchReviewCategory from '@/components/lunch/LunchReviewCategory';
+import LunchPhoto from '@/components/lunch/LunchPhoto';
+
+function LunchCalenderEatOut() {
+  const [isChecked, setIsChecked] = useState(false);
+  const checkboxRef = useRef<HTMLInputElement | null>(null);
+  const { subTitle, category, button } = CALENDAR_CONTENT;
+  const onClickHandler = () => {
+    console.log('onClickHandler');
+  };
+  return (
+    <section>
+      <Header title={category.eatOut.text} onClick={onClickHandler} />
+      <LunchSubTitle title={subTitle.todayLunch} type="title" />
+      <div css={reviewStyles}>
+        <div css={textStyles}>
+          <strong>식당 이름</strong>
+          <p>식당 메뉴</p>
+        </div>
+        <div css={ratingStyles}>
+          <LunchRatingButton
+            title={button.button5.good}
+            onClick={onClickHandler}
+            isChecked={isChecked}
+          />
+          <LunchRatingButton
+            title={button.button5.bad}
+            onClick={onClickHandler}
+            isChecked={isChecked}
+          />
+        </div>
+        <div>
+          <LunchReviewCategory title={category.eatOut.subCategory.amount} />
+          <LunchReviewCategory title={category.eatOut.subCategory.speed} />
+          <LunchReviewCategory title={category.eatOut.subCategory.service} />
+        </div>
+      </div>
+      <div css={textReviewStyles}>
+        <p>{subTitle.review}</p>
+        <Input
+          variant="search"
+          textarea
+          placeholder={category.eatOut.placeholder}
+          maxLength={299}
+        />
+      </div>
+      <LunchPhoto />
+      <Button content={button.button4.text2} variant="blue" />
+    </section>
+  );
+}
+
+const reviewStyles = css`
+  margin: 16px 0 20px;
+  padding: 16px 20px;
+`;
+
+const textStyles = css`
+  text-align: center;
+
+  strong {
+    font: ${theme.font.subTitle.subTitle1_600};
+    color: ${theme.palette.greyscale.grey90};
+  }
+
+  p {
+    font: ${theme.font.body.body2_400};
+    color: ${theme.palette.greyscale.grey60};
+  }
+`;
+
+const ratingStyles = css`
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+  margin: 12px 0;
+  padding: 12px 0;
+`;
+
+const textReviewStyles = css`
+  margin: 20px 0 16px;
+  p {
+    margin-bottom: 12px;
+    font: ${theme.font.subTitle.subTitle2_500}
+    color: ${theme.palette.greyscale.grey90};
+  }
+`;
+
+export default LunchCalenderEatOut;
