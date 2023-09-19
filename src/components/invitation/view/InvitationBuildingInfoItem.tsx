56ebebb9ceb3904434b540fac962a88280188e2f
Kim JinWoo <kimjinu2540@gmail.com>
2023-09-19 11:15:51 +0900

    feat: 빌딩 정보 관련 컴포넌트 추가

---
+++ src/components/invitation/view/InvitationBuildingInfoItem.tsx
@@ -0,0 +1,30 @@
+import theme from '@/styles/theme';
+import { InvitationBuildingInfoItemProps } from '@/types/invitation/view';
+import { css } from '@emotion/react';
+
+function InvitationBuildingInfoItem({
+  item,
+  content,
+}: InvitationBuildingInfoItemProps) {
+  return (
+    <div css={InfoItemStyles}>
+      <p>
+        <span>{item}</span>
+      </p>
+      <p>{content}</p>
+    </div>
+  );
+}
+
+const InfoItemStyles = css`
+  display: flex;
+  gap: 16px;
+  margin-bottom: 10px;
+
+  font: ${theme.font.body.body1_400};
+  span {
+    color: ${theme.palette.greyscale.grey50};
+  }
+`;
+
+export default InvitationBuildingInfoItem;
