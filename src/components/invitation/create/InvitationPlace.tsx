fb0e91458de4617b8b005a21e2dc223c9fc64742
junehee <dev.junehee@gmail.com>
2023-09-20 21:43:17 +0900

    design: 초대 장소 바텀시트 컴포넌트 레이아웃 구현

---
+++ src/components/invitation/create/InvitationPlace.tsx
@@ -0,0 +1,75 @@
+import Button from '@/components/common/Button';
+import RadioBtn from '@/components/common/RadioBtn';
+import CREATE_TEXTS from '@/constants/invitation/createTexts';
+import theme from '@/styles/theme';
+import mq from '@/utils/mediaquery';
+import { css } from '@emotion/react';
+
+function InvitationPlace() {
+  const { title, button, placeholder } = CREATE_TEXTS;
+
+  // const test = [{ main: '식스센스 사무실', sub: '2023.08.29' }];
+  const test2 = [
+    '식스센스 사무실 (10층 1004호)',
+    '공용라운지 (A동 1001호)',
+    '10층 회의실 A',
+    '10층 회의실 B',
+  ];
+
+  return (
+    <div css={containerStyles}>
+      <div css={titleStyles}>{title.invitationPlace}</div>
+      <div css={radioBtnWrapperStyles}>
+        <RadioBtn
+          list={test2}
+          name="초대 장소"
+          placeholder={placeholder.placeEtc}
+        />
+      </div>
+      <div css={buttonWrapperStyles}>
+        <Button variant="blue" content={button.save} />
+      </div>
+    </div>
+  );
+}
+
+const containerStyles = css`
+  display: flex;
+  flex-direction: column;
+  /* align-items: center; */
+  width: 100%;
+`;
+
+const radioBtnWrapperStyles = css`
+  /* width: 356px; */
+  /* overflow: scroll; */
+`;
+
+const titleStyles = css`
+  width: 100%;
+  color: ${theme.palette.title};
+  font: ${theme.font.title.title2_600};
+  line-height: 29px;
+  text-align: left;
+`;
+
+const buttonWrapperStyles = css`
+  min-width: 280px;
+  max-width: 360px;
+  padding-bottom: 20px;
+
+  ${mq.md} {
+    min-width: 361px;
+    max-width: 480px;
+  }
+  ${mq.lg} {
+    min-width: 481px;
+    max-width: 640px;
+  }
+  ${mq.tab} {
+    min-width: 641px;
+    max-width: 800px;
+  }
+`;
+
+export default InvitationPlace;
