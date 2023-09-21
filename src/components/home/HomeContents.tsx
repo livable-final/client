83b62d11ea9ef2d0a49c3997e782e4cf31dc4584
Kled <1017yu@daum.net>
2023-09-21 19:26:29 +0900

    feat: 홈파트 헤더 제외 컨텐츠 부분 컨테이너 생성

---
+++ src/components/home/HomeContents.tsx
@@ -0,0 +1,34 @@
+import { css } from '@emotion/react';
+import HomeBanner from '@/components/home/banner';
+import HomeBulletin from '@/components/home/bulletin';
+import { DUMMY_RESPONSE } from '@/constants/home/homeTexts';
+import HomeCafeteria from '@/components/home/cafeteria';
+import HomeService from '@/components/home/service';
+import HomeReception from '@/components/home/reception';
+import HomeServey from '@/components/home/HomeServey';
+
+function HomeContents() {
+  const { hasCafeteria } = DUMMY_RESPONSE;
+
+  return (
+    <div css={containerStyles}>
+      <HomeBanner hasCafeteria={hasCafeteria} />
+      <HomeBulletin />
+      {hasCafeteria && <HomeCafeteria />}
+      <HomeService />
+      <HomeReception />
+      <HomeServey />
+    </div>
+  );
+}
+
+const containerStyles = css`
+  display: flex;
+  padding: 12px 16px;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  align-self: stretch;
+`;
+
+export default HomeContents;
