3a11c8c510363bc5b6148505d47a680c3b8c9059
Kled <1017yu@daum.net>
2023-09-21 19:34:23 +0900

    design: 홈 서비스 파트 UI 작업

---
+++ src/components/home/service/HomeServiceCard.tsx
@@ -0,0 +1,39 @@
+import Icons from '@/components/common/Icons';
+import theme from '@/styles/theme';
+import { HomeServiceProps } from '@/types/home';
+import mq from '@/utils/mediaquery';
+import { css } from '@emotion/react';
+import Link from 'next/link';
+
+function HomeServiceCard({ icon, title, href }: HomeServiceProps) {
+  return (
+    <Link href={href} css={containerStyles}>
+      <Icons icon={icon} size="32" />
+      <span css={titleStyles}>{title}</span>
+    </Link>
+  );
+}
+
+const containerStyles = css`
+  display: flex;
+  height: 87px;
+  padding: 12px auto;
+  width: 72px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  border-radius: 12px;
+
+  ${mq.md} {
+    width: 91px;
+  }
+`;
+
+const titleStyles = css`
+  font: ${theme.font.body.body2_400};
+  color: ${theme.palette.greyscale.grey60};
+  line-height: 22px;
+`;
+
+export default HomeServiceCard;
