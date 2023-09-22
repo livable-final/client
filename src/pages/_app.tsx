0a8d1811430c8027ca9be729ab5788cf9c5585e2
Kled <1017yu@daum.net>
2023-09-22 14:30:44 +0900

    feat: react-query 프로바이더 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,22 @@ import Layout from '@/components/common/Layout';
 import GlobalStyle from '@/styles/GlobalStyle';
 import { ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
