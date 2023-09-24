7d1b70835ce8d1aa72eb4c67da3c58bdc96438ed
Kled <1017yu@daum.net>
2023-09-24 10:15:41 +0900

    design: 폰트 추가

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,10 +1,10 @@
 import { css } from '@emotion/react';
-import { pretendard, godo } from '@/styles/font';
+import { pretendard, godo, gwPower, yeongdeok } from '@/styles/font';
 
 function Layout({ children }: React.PropsWithChildren) {
   return (
     <div
-      className={`${pretendard.variable} ${godo.variable}`}
+      className={`${pretendard.variable} ${godo.variable} ${gwPower.variable} ${yeongdeok.variable}`}
       css={layoutStyles}
     >
       {children}
