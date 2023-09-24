562d68f9ef833260fb406a87dc4780d3973619d0
Kled <1017yu@daum.net>
2023-09-24 10:30:24 +0900

    feat: 오점완 메인 상수 지정

--- src/constants/lunch/index.ts
+++ src/constants/lunch/index.ts
@@ -72,3 +72,13 @@ export const CALENDAR_CASE = {
     type3: 'menu',
   },
 };
+
+export const LUNCH_MAIN_CONSTANTS = {
+  ranking: {
+    title: '지난주 점심 메뉴 TOP 10',
+    heights: [32, 47, 25],
+  },
+  reviews: {
+    title: '최근 테라타워 입주자들의 리뷰',
+  },
+};
