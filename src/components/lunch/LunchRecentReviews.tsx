26f6b73aaac40a107a7585a86764bd8d5cd24ad2
Kled <1017yu@daum.net>
2023-09-24 10:18:51 +0900

    design: 최근 리뷰 컨테이너 UI 작업

---
+++ src/components/lunch/LunchRecentReviews.tsx
@@ -0,0 +1,27 @@
+import Card from '@/components/common/Card';
+import { LUNCH_MAIN_CONSTANTS } from '@/constants/lunch';
+import DUMMY_DATA from '@/constants/lunch/dummy';
+import theme from '@/styles/theme';
+import { css } from '@emotion/react';
+import LunchReview from '@/components/lunch/review/LunchReview';
+
+function LunchRecentReviews() {
+  return (
+    <Card col>
+      <span css={titleStyles}>{LUNCH_MAIN_CONSTANTS.reviews.title}</span>
+      {DUMMY_DATA.map((item) => (
+        <LunchReview key={item.reveiwId} {...item} />
+      ))}
+    </Card>
+  );
+}
+
+const titleStyles = css`
+  flex: 1 0 0;
+  font: ${theme.font.body.body1_600};
+  color: ${theme.palette.title};
+  line-height: 24px;
+  padding-top: 16px;
+`;
+
+export default LunchRecentReviews;
