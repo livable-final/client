b1116e253d7ecf3977970f74cb1b95c5d199771e
Kled <1017yu@daum.net>
2023-09-25 14:21:03 +0900

    feat: 오점완 리뷰 상세 컨텐츠 컴포넌트 생성

---
+++ src/components/lunch/detail/LunchDetailContents.tsx
@@ -0,0 +1,48 @@
+import { css } from '@emotion/react';
+import LunchReview from '@/components/lunch/review/LunchReview';
+import useReviewStore from '@/stores/useReviewStore';
+import LunchSubTitle from '@/components/lunch/LunchSubTitle';
+import { LUNCH_MAIN_CONSTANTS } from '@/constants/lunch';
+import DUMMY_DATA from '@/constants/lunch/dummy';
+import theme from '@/styles/theme';
+
+function LunchDetailContents() {
+  const { reviewList } = useReviewStore();
+  const { detail } = LUNCH_MAIN_CONSTANTS;
+
+  return (
+    <div css={wrapperStyles}>
+      <LunchSubTitle
+        title={`${reviewList.restaurantName}${detail.title}`}
+        type="more"
+      />
+      <div css={reviewStyles}>
+        {DUMMY_DATA.map((item) => (
+          <LunchReview key={item.reveiwId} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const wrapperStyles = css`
+  margin: 0 -16px;
+  padding: 24px 0;
+
+  > div {
+    &:first-of-type {
+      margin-left: 16px;
+    }
+  }
+`;
+
+const reviewStyles = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-self: stretch;
+  background: ${theme.palette.white};
+  padding-right: 16px;
+`;
+
+export default LunchDetailContents;
