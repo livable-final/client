396da7de91e814c688b07729c80de67caa9ddc13
Siwoo Lee <125563995+cuconveniencestore@users.noreply.github.com>
2023-09-26 01:04:46 +0900

    design: 초대 목록 바텀 시트, 삭제 모달 구현 (#104)
    * design: 초대 목록 바텀 시트 생성
    
    * design: 초대목록 문자열 상수화
    
    * feat: 초대목록 바텀시트 연결

---
+++ src/components/user/UserInvitationListEdit.tsx
@@ -0,0 +1,68 @@
+import { css } from '@emotion/react';
+import {
+  USER_INVITATIONLIST_TEXT,
+  USER_INVITATIONLIST_BUTTON_TEXT,
+} from '@/constants/user/userInvitationTexts';
+import theme from '@/styles/theme';
+import Modal from '@/components/common/Modal';
+import useModalStore from '@/stores/useModalStore';
+
+function UserInvitationListEdit() {
+  const { title, confirm } = USER_INVITATIONLIST_BUTTON_TEXT;
+  const { modalState, openModal } = useModalStore();
+  const onclickdeleteHandler = () => {
+    openModal(`${confirm.title}`, `${confirm.message}`);
+  };
+
+  return (
+    <div css={userInvitationListEditStyles}>
+      <div css={titleStyles}>{USER_INVITATIONLIST_TEXT.edit}</div>
+      <div css={editSheetStyles}>
+        <button type="button" css={editButtonStyles}>
+          {title.edit}
+        </button>
+        <button
+          type="button"
+          css={deleteButtonStyles}
+          onClick={onclickdeleteHandler}
+        >
+          {title.delete}
+        </button>
+      </div>
+      {modalState.isOpen && <Modal isConfirm />}
+    </div>
+  );
+}
+const userInvitationListEditStyles = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const titleStyles = css`
+  margin-bottom: 20px;
+  font: ${theme.font.subTitle.subTitle2_600};
+  color: ${theme.palette.title};
+`;
+const editSheetStyles = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin-bottom: 20px;
+  width: 100%;
+`;
+const editButtonStyles = css`
+  display: flex;
+  padding: 16px 24px;
+  color: ${theme.palette.greyscale.grey60};
+  font: ${theme.font.subTitle.subTitle2_500};
+  line-height: 24px;
+`;
+const deleteButtonStyles = css`
+  display: flex;
+  padding: 16px 24px;
+  color: ${theme.palette.state.danger};
+  font: ${theme.font.subTitle.subTitle2_500};
+  line-height: 24px;
+`;
+export default UserInvitationListEdit;
---
+++ src/constants/user/userInvitationTexts.ts
@@ -0,0 +1,9 @@
+export const USER_INVITATIONLIST_BUTTON_TEXT = {
+  title: { edit: '수정하기', delete: '삭제하기' },
+  confirm: { title: '초대장 삭제', message: '초대장을 정말로 삭제 할까요?' },
+};
+
+export const USER_INVITATIONLIST_TEXT = {
+  title: '초대 목록',
+  edit: '초대 내역',
+};
