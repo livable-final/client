a262430a925336ab47d9a08e5b994197ce5bbada
Kled <1017yu@daum.net>
2023-09-26 14:17:15 +0900

    feat: 사진 배열 row-col props 추가

--- src/components/lunch/review/LunchReviewPhotos.tsx
+++ src/components/lunch/review/LunchReviewPhotos.tsx
@@ -1,18 +1,21 @@
+import { LUNCH_MAIN_CONSTANTS } from '@/constants/lunch';
 import theme from '@/styles/theme';
+import { ReviewPhotosProps } from '@/types/lunch/reviewPhotos';
 import { css } from '@emotion/react';
 import Image from 'next/image';
 
-function LunchReviewPhotos({ photos }: { photos: string[] }) {
+function LunchReviewPhotos({ photos, isRow }: ReviewPhotosProps) {
+  const { review, ranking } = LUNCH_MAIN_CONSTANTS;
   return (
-    <div css={containerStyles}>
+    <div css={containerStyles(isRow)}>
       <div css={wrapperStyles}>
         {photos.map((item, i) => (
           <Image
             key={item}
             src={item}
             alt={`이미지${i}`}
-            width={115}
-            height={90}
+            width={isRow ? ranking.photoWidth : review.photoWidth}
+            height={isRow ? ranking.photoHeight : review.photoHeight}
           />
         ))}
       </div>
@@ -20,11 +23,12 @@ function LunchReviewPhotos({ photos }: { photos: string[] }) {
   );
 }
 
-const containerStyles = css`
+const containerStyles = (isRow?: boolean) => css`
   display: flex;
-  overflow-x: auto;
+  overflow-x: ${isRow ? 'visible' : 'auto'};
   max-width: 100%;
   padding-right: -16px;
+  object-fit: cover;
 `;
 
 const wrapperStyles = css`
