c161bc7fe0d60c10b596fab8f6c09b34ab40d628
Kled <1017yu@daum.net>
2023-09-26 20:54:57 +0900

    feat: 홈 구내식당 토글 기능 추가

--- src/components/home/cafeteria/HomeCafeteriaToggle.tsx
+++ src/components/home/cafeteria/HomeCafeteriaToggle.tsx
@@ -1,21 +1,44 @@
+import { HOME_TEXTS } from '@/constants/home/homeTexts';
 import theme from '@/styles/theme';
+import { HomeCafeteriaToggleProps } from '@/types/home';
 import { css } from '@emotion/react';
 
-function HomeCafeteriaToggle({ type }: { type: string }) {
-  return <div css={containerStyles}>{type}</div>;
+function HomeCafeteriaToggle({
+  type,
+  onToggle,
+  isActive,
+}: HomeCafeteriaToggleProps) {
+  const mealType = HOME_TEXTS.cafeteria.type;
+  const onClickHandler = () => {
+    onToggle(mealType.indexOf(type));
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={onClickHandler}
+      disabled={isActive}
+      css={containerStyles(isActive)}
+    >
+      {type}
+    </button>
+  );
 }
 
-const containerStyles = css`
+const containerStyles = (isActive: boolean) => css`
   display: flex;
   padding: 4px 8px;
   justify-content: center;
   align-items: center;
   gap: 4px;
   border-radius: 100px;
-  background: ${theme.palette.greyscale.grey10};
   font: ${theme.font.body.body4};
-  color: ${theme.palette.greyscale.grey50};
   line-height: 16px;
+  cursor: pointer;
+  background: ${isActive
+    ? theme.palette.bluescale.blue40
+    : theme.palette.greyscale.grey10};
+  color: ${isActive ? theme.palette.white : theme.palette.greyscale.grey50};
 `;
 
 export default HomeCafeteriaToggle;
