7b3d430c886d4f2396a07fa9d42d0389fd9e8610
Kled <1017yu@daum.net>
2023-09-27 17:06:07 +0900

    feat: Main 페이지 컴포넌트 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,53 @@
-import Bnb from '@/components/common/Bnb';
-import Title from '@/components/common/Title';
-import HomeContents from '@/components/home/HomeContents';
-import { DUMMY_RESPONSE } from '@/constants/home/homeTexts';
-import theme from '@/styles/theme';
+import AuthForm from '@/components/auth/AuthForm';
+import useViewStore from '@/stores/usePagesStore';
+import mq from '@/utils/mediaquery';
 import { css } from '@emotion/react';
-import React from 'react';
+import Home from '@/components/home';
+import COMPONENT_NAME from '@/constants/common/pages';
+import { useEffect, useState } from 'react';
+import Title from '@/components/common/Title';
 
-function Home() {
+function Main() {
+  const { nextComponent } = useViewStore();
+  const { home } = COMPONENT_NAME.common;
+  const [userToken, setUserToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('user');
+    setUserToken(token);
+  }, []);
+
+  if (nextComponent === home || userToken) {
+    return <Home />;
+  }
+
   return (
-    <>
-      <Title title={DUMMY_RESPONSE.buildingName} part="main" isMain />
-      <div css={containerStyles}>
-        <HomeContents />
-      </div>
-      <Bnb />
-    </>
+    <div css={containerStyles}>
+      <Title title="로그인" part="login" />
+      <AuthForm />
+    </div>
   );
 }
 
 const containerStyles = css`
-  margin: 0 -16px 90px;
-  background: ${theme.palette.background.home};
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  gap: 96px;
+  min-width: 280px;
+  max-width: 360px;
+
+  ${mq.md} {
+    max-width: 480px;
+  }
+  ${mq.lg} {
+    max-width: 640px;
+  }
+  ${mq.tab} {
+    max-width: 1024px;
+  }
 `;
 
-export default Home;
+export default Main;
