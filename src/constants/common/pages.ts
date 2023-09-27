735eb0f2910b45c43879568c6cf081f03876432f
Kled <1017yu@daum.net>
2023-09-27 17:06:24 +0900

    feat: 컴포넌트명 상수 추가

---
+++ src/constants/common/pages.ts
@@ -0,0 +1,30 @@
+const COMPONENT_NAME = {
+  invitation: {
+    create: {
+      visiors: 'InvitationVisitorsContainer',
+      info: 'InvitationInfoContainer',
+      done: 'InvitationDoneContainer',
+      purpose: 'InvitationPurposeContainer',
+    },
+    view: {},
+  },
+  lunch: {
+    calendar: {
+      review: 'LunchCalendarReview',
+      menu: 'LunchCalendarMenu',
+      search: 'LunchCalendarSearch',
+      cafeteria: 'LunchCalendarCafeteria',
+      lunchBox: 'LunchCalendarLunchBox',
+    },
+    detail: {
+      detail: 'LunchDetail',
+      reviewByRanking: 'LunchReviewsByRanking',
+    },
+  },
+  common: {
+    signIn: 'SignIn',
+    home: 'home',
+  },
+};
+
+export default COMPONENT_NAME;
