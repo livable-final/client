ed78e55889f7783935b86574e37b8ba14fc7bda3
Kled <1017yu@daum.net>
2023-09-29 08:55:34 +0900

    feat: 특정 메뉴 판매 식당 목록 API 호출 함수 추가

--- src/pages/api/lunch/lunchRequests.ts
+++ src/pages/api/lunch/lunchRequests.ts
@@ -1,27 +1,36 @@
 import { Response } from '@/types/common/response';
 import { apiInstance } from '@/pages/api/axios';
 import {
-  GetMenuReviewsProps,
-  GetMenusProps,
-  GetRankingProps,
-} from '@/types/lunch/response';
+  GetMenuReviewsData,
+  GetMenusData,
+  GetRankingData,
+  GetRestListData,
+} from '@/types/lunch/api';
 
-export const getMenus = async (): Promise<Response<GetMenusProps[]>> => {
-  const response = await apiInstance.get('menus');
+export const getMenus = async (): Promise<Response<GetMenusData[]>> => {
+  const response = await apiInstance.get(`menus`);
   return response.data;
 };
 
 // 아직 데이터 없음. 사용 불가
 // * GET 가장 많이 선택한 메뉴 10위까지의 응답
-export const getRanking = async ({ buildingId }: GetRankingProps) => {
+export const getRanking = async ({ buildingId }: GetRankingData) => {
   const response = await apiInstance.get(`menus/buildings/${buildingId}`);
   return response.data;
 };
 
 // * GET 특정 메뉴에 대한 리뷰 리스트 응답
-export const getMenuReviews = async ({ menuId, page }: GetMenuReviewsProps) => {
+export const getMenuReviews = async ({ menuId, page }: GetMenuReviewsData) => {
   const response = await apiInstance.get(
     `reviews/menus/${menuId}?page=${page}&size=10`,
   );
+  return response.data;
+};
+
+// * GET 특정 메뉴를 판매하는 식당 목록 응답
+export const getRestList = async (
+  menuId?: number | string | Date,
+): Promise<Response<GetRestListData[]>> => {
+  const response = await apiInstance.get(`restaurants/menus/${menuId}`);
   return response.data;
 };
