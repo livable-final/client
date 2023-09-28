dabd9a005016452e30850fb683cd2f587cfdee2c
ChoEun-Sang <wh94es@gmail.com>
2023-09-29 02:17:25 +0900

    refactor: 외식 - 식당 검색 컴포넌트 api 연결 및 더미데이터 테스트

--- src/components/lunch/calendar/LunchCalendarSearch.tsx
+++ src/components/lunch/calendar/LunchCalendarSearch.tsx
@@ -1,22 +1,54 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { css } from '@emotion/react';
 import { SearchIcon } from '@/assets/icons';
 import { CALENDAR_CONTENT, CALENDAR_CASE } from '@/constants/lunch';
+import COMPONENT_NAME from '@/constants/common/pages';
+import { getRestaurants } from '@/pages/api/lunch/calendarRequests';
+import { RestaurantsData } from '@/types/lunch/calendar';
 import theme from '@/styles/theme';
 import Header from '@/components/common/Header';
 import Input from '@/components/common/Input';
 import LunchCalendarListItem from '@/components/lunch/calendar/LunchCalendarListItem';
-import LunchCalendarMenu from '@/components/lunch/calendar/LunchCalendarMenu';
 import usePagesStore from '@/stores/usePagesStore';
+import useWriteStore from '@/stores/useWriteStore';
+import useFetch from '@/hooks/useFetch';
 
 function LunchCalendarSearch() {
-  const [searchText, setSearchText] = useState('');
-  const { nextComponent, setNextComponent } = usePagesStore();
+  const [searchData, setSearchData] = useState<RestaurantsData[]>([]);
+  const [showSearch, setShowSearch] = useState(true);
+  const [keyword, setKeyword] = useState('');
+  const { nextComponent, setNextComponent, goBack } = usePagesStore();
+  const setRestaurant = useWriteStore((state) => state.setRestaurant);
+
+  const { calendar } = COMPONENT_NAME.lunch;
   const { title, subTitle } = CALENDAR_CONTENT;
   const { listItem } = CALENDAR_CASE;
 
+  useEffect(() => {}, []);
+
   const onClickHeaderHandler = () => {
-    setNextComponent('LunchCalendarReview');
+    goBack();
+  };
+
+  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      console.log('keyword', keyword);
+      const res = await getRestaurants(keyword);
+      setSearchData(res);
+      console.log('검색 성공', res);
+    } catch (err) {
+      console.log('검색 오류', err);
+    }
+  };
+  const onClickBtnHandler = (
+    e: MouseEvent,
+    restaurantId: number,
+    restaurantName: string,
+  ) => {
+    setNextComponent(calendar.menu);
+    setRestaurant({ restaurantId, restaurantName });
   };
 
   const onChangeHandler = (
@@ -24,49 +56,81 @@ function LunchCalendarSearch() {
       | React.ChangeEvent<HTMLInputElement>
       | React.ChangeEvent<HTMLTextAreaElement>,
   ) => {
-    setSearchText(e.target.value);
+    e.preventDefault();
+
+    setKeyword(e.target.value);
   };
 
-  if (nextComponent === 'LunchCalendarMenu') return <LunchCalendarMenu />;
+  const searchData1 = [
+    {
+      restaurantId: 1,
+      restaurantName: '김밥천국',
+      restaurantCategory: '분식',
+      inBuilding: false,
+      estimatedTime: 15,
+      foor: 0,
+    },
+    {
+      restaurantId: 2,
+      restaurantName: '할머니순두부',
+      restaurantCategory: '한식',
+      inBuilding: true,
+      estimatedTime: 0,
+      floor: -1, // (지하 1층)
+    },
+    {
+      restaurantId: 3,
+      restaurantName: '제육한상',
+      restaurantCategory: '한식',
+      inBuilding: false,
+      estimatedTime: 3,
+      floor: 0,
+    },
+  ];
 
   return (
     <section>
       <Header title={title.search} onClick={onClickHeaderHandler} />
-      <div css={inputStyles}>
-        <Input variant="search" value={searchText} onChange={onChangeHandler} />
-        <SearchIcon css={iconStyles} />
-      </div>
-      <div css={textStyles}>
-        <span>{subTitle.recentSearches}</span>
-        <div>
-          <LunchCalendarListItem
-            type={listItem.type1}
-            content="최근 검색한 결과 (테스트용)"
-          />
+      <form css={inputStyles} onSubmit={onSubmitHandler}>
+        <Input variant="search" value={keyword} onChange={onChangeHandler} />
+        <button type="submit" css={iconStyles}>
+          <SearchIcon />
+        </button>
+      </form>
+
+      {!showSearch ? (
+        <div css={textStyles}>
+          <span>{subTitle.recentSearches}</span>
+          <div>
+            <LunchCalendarListItem
+              type={listItem.type1}
+              content="최근 검색한 결과 (테스트용)"
+            />
+          </div>
         </div>
-      </div>
-      <div>
+      ) : (
         <div>
-          <span css={textStyles}>{subTitle.searchResults}</span>
-          <span css={colorTextStyles}>3건</span>
+          <div>
+            <span css={textStyles}>{subTitle.searchResults}</span>
+            <span css={colorTextStyles}>3건</span>
+          </div>
+          <div>
+            {searchData1.map((item) => (
+              <LunchCalendarListItem
+                key={item.restaurantId}
+                type={listItem.type2}
+                content={item.restaurantName}
+                category={item.restaurantCategory}
+                time={item.estimatedTime}
+                imageUrl="/ruppy.png"
+                onClick={(e) =>
+                  onClickBtnHandler(e, item.restaurantId, item.restaurantName)
+                }
+              />
+            ))}
+          </div>
         </div>
-        <div>
-          <LunchCalendarListItem
-            type={listItem.type2}
-            content="검색한 결과 (테스트용)"
-            category="한식 (테스트용)"
-            time={3}
-            imageUrl="/gift.png"
-          />
-          <LunchCalendarListItem
-            type={listItem.type2}
-            content="검색한 결과 (테스트용)"
-            category="한식 (테스트용)"
-            time={3}
-            imageUrl="/gift.png"
-          />
-        </div>
-      </div>
+      )}
     </section>
   );
 }
@@ -82,7 +146,6 @@ const iconStyles = css`
   bottom: 0;
   right: 16px;
   margin: auto 0;
-  cursor: auto;
 `;
 
 const textStyles = css`
