4663ea45c929b085475d04dca2d89daf26e0fa29
junehee <dev.junehee@gmail.com>
2023-09-30 00:51:21 +0900

    refactor: getPlaceId 타입 추가

--- src/utils/getPlaceId.ts
+++ src/utils/getPlaceId.ts
@@ -1,11 +1,7 @@
+import { PlaceList } from '@/types/invitation/create';
+
 // arr : value가 포함된 객체로 이루어진 배열
 // value : 찾으려는 ID를 가지고 있는 값
-
-interface PlaceList {
-  officeName?: string;
-  commonPlaceName?: string;
-  commonPlaceId?: number;
-}
 
 const getPlaceId = (arr: PlaceList[], value: string) => {
   const found = arr.find((obj) => obj.commonPlaceName === value);
