54891ace031acf95edd92dcd47b6934a6fe6bc5c
ChoEun-Sang <wh94es@gmail.com>
2023-09-30 19:34:04 +0900

    feat: 게시글 작성 상태 관리 Store 생성

--- src/stores/useWriteStore.ts
+++ src/stores/useWriteStore.ts
@@ -5,15 +5,38 @@ type SelectedDate = {
   restaurantName: string;
 };
 
+export interface MenuData {
+  menuId: number;
+  menuName: string;
+}
+
+type RatingDate = {
+  taste?: string;
+  amount?: string;
+  speed?: string;
+  service?: string;
+};
+
+interface SetRatingData {
+  [key: string]: string;
+}
+
 type WriteStore = {
   restaurant: SelectedDate;
-  selectedMenu: string[];
+  selectedMenu: MenuData[];
+  ratingState: RatingDate;
+  description: string;
+  imageFiles: File[];
   isChecked: boolean;
   setRestaurant: (data: SelectedDate) => void;
-  setSelectedMenu: (data: string) => void;
-  setRemoveMenu: (data: string) => void;
+  setSelectedMenu: (data: MenuData) => void;
+  setRemoveMenu: (data: MenuData) => void;
+  setRatingState: (data: SetRatingData) => void;
+  setDescription: (data: string | '') => void;
+  setImageFiles: (files: File[]) => void;
   setIsChecked: (data: boolean) => void;
-  clear: () => void;
+  clearRestaurant: () => void;
+  resetSelectedMenu: () => void;
 };
 
 const initialState = {
@@ -21,19 +44,42 @@ const initialState = {
   restaurantName: '',
 };
 
+const initialArray: MenuData[] = [];
+
+const initialRatingState = {
+  taste: '',
+  amount: 'BAD',
+  speed: 'BAD',
+  service: 'BAD',
+};
+
 const useWriteStore = create<WriteStore>()((set) => ({
   restaurant: initialState,
-  selectedMenu: [],
+  selectedMenu: initialArray,
+  ratingState: initialRatingState,
+  description: '',
+  imageFiles: [],
   isChecked: false,
   setRestaurant: (data) => set({ restaurant: data }),
   setSelectedMenu: (data) =>
     set((pre) => ({ selectedMenu: [...pre.selectedMenu, data] })),
   setRemoveMenu: (data) =>
     set((pre) => ({
-      selectedMenu: [...pre.selectedMenu.filter((menu) => menu !== data)],
+      selectedMenu: [
+        ...pre.selectedMenu.filter((menu) => menu.menuName !== data.menuName),
+      ],
     })),
+  setRatingState: (data) =>
+    set((pre) => ({ ratingState: { ...pre.ratingState, ...data } })),
+  setDescription: (data) => set({ description: data }),
+  setImageFiles: (selectedFiles) => set({ imageFiles: selectedFiles }),
   setIsChecked: (data) => set({ isChecked: !data }),
-  clear: () => set({ restaurant: initialState }),
+  clearRestaurant: () => set({ restaurant: initialState }),
+  resetSelectedMenu: () => {
+    set({
+      selectedMenu: initialArray,
+    });
+  },
 }));
 
 export default useWriteStore;
