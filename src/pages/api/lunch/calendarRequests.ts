9a36c65fd82b4b5b2883c7db7f7367602bf26b8b
ChoEun-Sang <wh94es@gmail.com>
2023-09-30 19:44:54 +0900

    refactor: api url수정

--- src/pages/api/lunch/calendarRequests.ts
+++ src/pages/api/lunch/calendarRequests.ts
@@ -1,48 +1,48 @@
 import { apiInstance } from '@/pages/api/axios';
 
 // 달력
-export const getReviewData = async (year: string, month: string) => {
+export const getReviewData = async (year: number, month: number) => {
   const res = await apiInstance.get(
-    `/api/reviews/members?year=${year}&month=${month}`,
+    `/reviews/members?year=${year}&month=${month}`,
   );
   return res.data;
 };
 
 // 리뷰 작성 - 외식
 export const getRestaurants = async (keyword: string) => {
-  const res = await apiInstance.get(`/api/restaurants/search?query=${keyword}`);
+  const res = await apiInstance.get(`/restaurants/search?query=${keyword}`);
   return res.data;
 };
 
 export const getRestaurantMenu = async (restaurantId: number) => {
-  const res = await apiInstance.get(`/api/restaurant/${restaurantId}/menus`);
+  const res = await apiInstance.get(`/restaurant/${restaurantId}/menus`);
   return res.data;
 };
 
 export const postRestaurantReview = async (body: FormData) => {
-  const res = await apiInstance.post(`/api/reviews/restaurant`, body);
+  const res = await apiInstance.post(`/reviews/restaurant`, body);
   return res;
 };
 
 // 리뷰작성 - 구내식당
 export const postCafeteriaReview = async (body: FormData) => {
-  const res = await apiInstance.post(`/api/reviews/cafeteria`, body);
+  const res = await apiInstance.post(`/reviews/cafeteria`, body);
   return res;
 };
 
 // 리뷰작성 - 도시락
 export const postLunchBoxReview = async (body: FormData) => {
-  const res = await apiInstance.post(`/api/reviews/lunch-box`, body);
+  const res = await apiInstance.post(`/reviews/lunch-box`, body);
   return res;
 };
 
 // 포인트
 export const getPointLogs = async () => {
-  const res = await apiInstance.get(`/api/points/logs/members`);
+  const res = await apiInstance.get(`/points/logs/members`);
   return res.data;
 };
 
 export const postPoint = async () => {
-  const res = await apiInstance.post(`/api/points/logs/members`);
+  const res = await apiInstance.post(`/points/logs/members`);
   return res;
 };
