d7b65af66ac77dedf6a108d426278722e00af758
Kled <1017yu@daum.net>
2023-09-30 22:03:51 +0900

    fix: table 변경에 따른 store 수정

--- src/stores/useSaveStore.ts
+++ src/stores/useSaveStore.ts
@@ -8,22 +8,29 @@ type Save = {
 type SaveStore = {
   isSave: Save;
   user: string;
+  visitor: string;
   setIsSavePhotoMsg: () => void;
   clearIsSave: () => void;
   setUserToken: (value: string) => void;
+  setVisitorToken: (value: string) => void;
 };
 
 const initialSaveState = {
   PhotoMsg: false,
 };
 
-const initialUserState = '';
+const initialTokenState = {
+  user: '',
+  visitor: '',
+};
 
 const useSaveStore = create<SaveStore>()(
   persist(
     (set) => ({
       isSave: initialSaveState,
-      user: initialUserState,
+      user: initialTokenState.user,
+      visitor: initialTokenState.visitor,
+
       setIsSavePhotoMsg: () =>
         set((pre) => ({
           isSave: { ...pre.isSave, PhotoMsg: true },
@@ -34,6 +41,10 @@ const useSaveStore = create<SaveStore>()(
       setUserToken: (value: string) =>
         set({
           user: value,
+        }),
+      setVisitorToken: (value: string) =>
+        set({
+          visitor: value,
         }),
     }),
     {
