1813d93693bf4fffab5c38cfdbd0056801a1be4e
junehee <dev.junehee@gmail.com>
2023-09-30 19:01:47 +0900

    feat: useSaveStore에 방문팁 메세지 저장 상태 추가

--- src/stores/useSaveStore.ts
+++ src/stores/useSaveStore.ts
@@ -5,22 +5,39 @@ type Save = {
   PhotoMsg: boolean;
 };
 
+type Visit = {
+  visitMsg: boolean;
+  visitMsgText: string;
+};
+
 type SaveStore = {
   isSave: Save;
   user: string;
   keyword: string[];
+  visit: Visit;
   setIsSavePhotoMsg: () => void;
   setKeyword: (data: string) => void;
   deleteKeyword: (data: string) => void;
   clearIsSave: () => void;
   setUserToken: (value: string) => void;
+  setIsSaveVisitMsg: (state: boolean) => void;
+  setVisitMsgText: (text: string) => void;
+  clearVisitMsg: () => void;
 };
 
+// 오늘점심 초기값
 const initialSaveState = {
   PhotoMsg: false,
 };
 
+// 사용자 토큰 초기값
 const initialUserState = '';
+
+// 초대장 생성 방문tip 초기값
+const initialVisitMsg = {
+  visitMsg: false,
+  visitMsgText: '',
+};
 
 const useSaveStore = create<SaveStore>()(
   persist(
@@ -28,6 +45,7 @@ const useSaveStore = create<SaveStore>()(
       isSave: initialSaveState,
       user: initialUserState,
       keyword: [],
+      visit: initialVisitMsg,
       setIsSavePhotoMsg: () =>
         set((pre) => ({
           isSave: { ...pre.isSave, PhotoMsg: true },
@@ -47,6 +65,18 @@ const useSaveStore = create<SaveStore>()(
         set({
           user: value,
         }),
+      setIsSaveVisitMsg: (state: boolean) =>
+        set((pre) => ({
+          visit: { ...pre.visit, visitMsg: state },
+        })),
+      setVisitMsgText: (text: string) => {
+        set((pre) => ({
+          visit: { ...pre.visit, visitMsgText: text },
+        }));
+      },
+      clearVisitMsg: () => {
+        set({ visit: initialVisitMsg });
+      },
     }),
     {
       name: 'user-storage',
