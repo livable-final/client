0b28916a3f335863668e6139c00a697d1c64e255
Kled <1017yu@daum.net>
2023-09-30 22:02:36 +0900

    feat: building 정보를 담는 store 기능 추가

--- src/components/lunch/review/LunchReviewsByRest.tsx
+++ src/components/lunch/review/LunchReviewsByRest.tsx
@@ -4,21 +4,28 @@ import LunchSubTitle from '@/components/lunch/LunchSubTitle';
 import { DUMMY_RESPONSE } from '@/constants/lunch/dummy';
 import LunchRest from '@/components/lunch/LunchRest';
 import theme from '@/styles/theme';
+import useRouletteStore from '@/stores/useRouletteStore';
 // import { getRestList } from '@/pages/api/lunch/lunchRequests';
 // import useFetch from '@/hooks/useFetch';
 // { menuId }: { menuId: number }
 
 function LunchReviewsByRest() {
   const { title } = LUNCH_ROULETTE_CONSTANTS;
+  const { menuState, isAgain, isOperated } = useRouletteStore();
   // const { response } = useFetch({
   //   fetchFn: getRestList,
   //   arg: menuId,
-  // });
+  const renderTitle = () => {
+    return isAgain && isOperated ? `"${menuState}"` : `${title.recent}`;
+  };
 
   return (
     <div css={wrapperStyles}>
       <div css={stickyStyles}>
-        <LunchSubTitle title={title.review} type="title" />
+        <LunchSubTitle
+          title={`${renderTitle()} ${title.review}`}
+          type="title"
+        />
       </div>
       {/* {response?.data.map((v) => v.address)} */}
       {DUMMY_RESPONSE.data.map((item) => (
@@ -36,16 +43,10 @@ const wrapperStyles = css`
 
 const stickyStyles = css`
   position: sticky;
-  width: calc(100% + 32px);
   padding: 16px 0;
-  margin: 0 -16px;
   top: 56px;
   left: 0;
   background-color: ${theme.palette.white};
-
-  > div {
-    padding-left: 16px;
-  }
 `;
 
 export default LunchReviewsByRest;
