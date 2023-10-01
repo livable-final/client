446896fcce3a8b6993283858c73990b909f34613
Siwoo Lee <siwoo28@kakao.com>
2023-10-01 19:31:01 +0900

    feat: 초대장 수정 api 관련 타입 수정

--- src/types/invitation/edit.ts
+++ src/types/invitation/edit.ts
@@ -1,3 +1,5 @@
+import { VisitorInfo } from '@/types/invitation/api';
+
 export interface AddVisitorName {
   visitorId: number;
   name: string;
@@ -19,4 +21,11 @@ export interface InvitationAddVisitorListProps {
   // 초대장 생성에서 기존 등록해서 state에 담았던 값들 가져오는 props  추후 api 연동 후 수정요망
   visitorsList?: NewVisitorsList[];
   setVisitorList?: React.Dispatch<React.SetStateAction<NewVisitorsList[]>>;
+}
+export interface PatchInvitationContents {
+  commonPlaceId?: number | null;
+  description?: string;
+  startDate?: Date | string;
+  endDate?: Date | string;
+  visitors?: VisitorInfo[];
 }
