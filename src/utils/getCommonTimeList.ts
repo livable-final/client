4715151349cef4340a0b632afd88ae9a4921c22a
junehee <dev.junehee@gmail.com>
2023-10-02 03:31:31 +0900

    feat: 배열에서 공통된 시간 출력하는 함수 구성

---
+++ src/utils/getCommonTimeList.ts
@@ -0,0 +1,22 @@
+import { GetInvitationTimeListData } from '@/types/invitation/api';
+
+// [{…}, {…}]
+// { date: '2023-10-20', availableTimes: ['12:30:00', '15:00:00', '17:30:00'] }
+const getCommonTimes = (timeList: GetInvitationTimeListData[]) => {
+  const timeCounts: Record<string, number> = {};
+
+  timeList.forEach((item: GetInvitationTimeListData) => {
+    item.availableTimes.forEach((time: string) => {
+      if (!timeCounts[time]) timeCounts[time] = 1;
+      else timeCounts[time]++;
+    });
+  });
+
+  const commonTimes = Object.keys(timeCounts).filter(
+    (time) => timeCounts[time] === timeList.length,
+  );
+
+  return commonTimes;
+};
+
+export default getCommonTimes;
