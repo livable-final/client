75e32a5798557777f53893ad1698581b6e426284
junehee <dev.junehee@gmail.com>
2023-10-01 14:14:36 +0900

    refactor: 주소록 공용 컴포넌트 버튼 태그로 변경, 클릭 이벤트 추가

--- src/components/common/AddressBook.tsx
+++ src/components/common/AddressBook.tsx
@@ -1,15 +1,24 @@
+import Modal from '@/components/common/Modal';
+import useModalStore from '@/stores/useModalStore';
 import theme from '@/styles/theme';
 import { css } from '@emotion/react';
 import { RightSmall } from '@/assets/icons';
 
 function AddressBook() {
+  const { modalState, openModal } = useModalStore();
+
+  const onClick = () => {
+    openModal('📢', '주소록 연결 기능 준비 중입니다.');
+  };
+
   return (
-    <div css={addressTextStyles}>
+    <button type="button" css={addressTextStyles} onClick={onClick}>
       <div>주소록에서 찾기</div>
-      <button type="button" css={iconStyles}>
+      <div css={iconStyles}>
         <RightSmall />
-      </button>
-    </div>
+      </div>
+      {modalState.isOpen && <Modal isAlert />}
+    </button>
   );
 }
 
@@ -18,6 +27,7 @@ const addressTextStyles = css`
   justify-content: flex-end;
   align-items: center;
   cursor: pointer;
+
   div {
     font: ${theme.font.body.body3_500};
     color: ${theme.palette.input.unabled};
