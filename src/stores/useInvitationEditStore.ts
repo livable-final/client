aa763ea6ec4a216fcda509814fd3dd883152c735
Siwoo Lee <siwoo28@kakao.com>
2023-10-01 19:32:13 +0900

    feat: 초대장 수정 데이터 스토어 생성

---
+++ src/stores/useInvitationEditStore.ts
@@ -0,0 +1,34 @@
+import { create } from 'zustand';
+import { PatchInvitationContents } from '@/types/invitation/edit';
+import { VisitorInfo } from '@/types/invitation/api';
+
+interface InvitationEditStore {
+  editContents: PatchInvitationContents;
+  setEditContents: (
+    key: string,
+    content: string | number | Date | VisitorInfo[] | null | undefined,
+  ) => void;
+}
+
+// 초대장 수정 데이터 초기값
+const initialEditState: PatchInvitationContents = {
+  commonPlaceId: null,
+  description: '',
+  startDate: '',
+  endDate: '',
+  visitors: [],
+};
+
+const useInvitationEditStore = create<InvitationEditStore>((set) => ({
+  editContents: initialEditState,
+  setEditContents: (key, content) => {
+    set((state) => ({
+      editContents: {
+        ...state.editContents,
+        [key]: content,
+      },
+    }));
+  },
+}));
+
+export default useInvitationEditStore;
