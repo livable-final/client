ef440ad3c9453e0ab3d645c72d36b650efa405b1
Siwoo Lee <siwoo28@kakao.com>
2023-10-02 01:02:23 +0900

    feat: 초대장 수정 api 등록

--- src/pages/api/invitation/editRequests.ts
+++ src/pages/api/invitation/editRequests.ts
@@ -3,6 +3,7 @@ import { Response } from '@/types/common/response';
 import {
   GetInvitationListData,
   GetInvitationListItemData,
+  PatchInvitationContents,
 } from '@/types/invitation/api';
 
 // 초대 목록
@@ -24,5 +25,16 @@ export const getInvitationListItem = async (
 // 초대 목록 삭제
 export const getInvitationDeleteList = async (id?: number | string | Date) => {
   const response = await apiInstance.delete(`invitation/${id}`);
+  return response;
+};
+// 초대장 수정
+export const getInvitationEditList = async (
+  invitationEditContents: PatchInvitationContents,
+  id?: number | string | Date,
+) => {
+  const response = await apiInstance.patch(
+    `invitation/${id}`,
+    invitationEditContents,
+  );
   return response;
 };
