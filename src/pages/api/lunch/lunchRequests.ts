607cba090cc0d93f3b8bedcc705847b6be3cbc15
Kled <1017yu@daum.net>
2023-10-03 00:35:39 +0900

    fix: API PATH 수정

--- src/pages/api/lunch/lunchRequests.ts
+++ src/pages/api/lunch/lunchRequests.ts
@@ -8,6 +8,7 @@ import {
   PostMenuContent,
 } from '@/types/lunch/api';
 
+// * GET 룰렛에 사용되는 카테고리, 메뉴 목록 응답
 export const getMenus = async (): Promise<Response<GetMenusData[]>> => {
   const response = await apiInstance.get(`menus`);
   return response.data;
@@ -15,12 +16,12 @@ export const getMenus = async (): Promise<Response<GetMenusData[]>> => {
 
 // 아직 데이터 없음. 사용 불가
 // * GET 가장 많이 선택한 메뉴 10위까지의 응답
-export const getRanking = async ({
-  buildingId,
-}: {
-  buildingId: number;
-}): Promise<Response<GetRankingData[]>> => {
-  const response = await apiInstance.get(`menus/buildings/${buildingId}`);
+export const getRanking = async (
+  buildingId: number,
+): Promise<Response<GetRankingData[]>> => {
+  const response = await apiInstance.get(
+    `menus/choices?buildingId=${buildingId}`,
+  );
   return response.data;
 };
 
@@ -35,35 +36,24 @@ export const getMenuReviews = async (
 // * GET 특정 메뉴를 판매하는 식당 목록 응답
 export const getRestList = async (
   menuId: number,
-  page?: number,
 ): Promise<Response<GetRestListData[]>> => {
-  const response = await apiInstance.get(
-    `restaurants/menus/${menuId}?page=${page}`,
-  );
+  const response = await apiInstance.get(`restaurants?menuId=${menuId}`);
   return response.data;
 };
 
 // * GET 최신 리뷰 리스트 응답
-export const getReviewList = async ({
-  buildingId,
-}: {
-  buildingId: number;
-}): Promise<Response<GetMenuReviewsData[]>> => {
+export const getReviewList = async (
+  buildingId: number,
+): Promise<Response<GetMenuReviewsData[]>> => {
   const response = await apiInstance.get(`reviews/buildings/${buildingId}`);
   return response.data;
 };
 
 // * GET 특정 음식점 리뷰 리스트 응답
-export const getRestReviewList = async ({
-  restaurantId,
-  page,
-}: {
-  restaurantId?: number;
-  page?: number;
-}): Promise<Response<GetMenuReviewsData[]>> => {
-  const response = await apiInstance.get(
-    `reviews/restaurants/${restaurantId}?page=${page}`,
-  );
+export const getRestReviewList = async (
+  restaurantId?: number,
+): Promise<Response<GetMenuReviewsData[]>> => {
+  const response = await apiInstance.get(`reviews/restaurants/${restaurantId}`);
   return response.data;
 };
 
