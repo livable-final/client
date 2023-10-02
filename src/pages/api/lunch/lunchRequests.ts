aea610cfe4ab38122b3d6cbd5e340b67fc37bb64
Kled <1017yu@daum.net>
2023-10-02 10:55:10 +0900

    refactor: 페이지 argument 임시 추가

--- src/pages/api/lunch/lunchRequests.ts
+++ src/pages/api/lunch/lunchRequests.ts
@@ -25,11 +25,9 @@ export const getRanking = async ({
 };
 
 // * GET 특정 메뉴에 대한 리뷰 리스트 응답
-export const getMenuReviews = async ({
-  menuId,
-}: {
-  menuId: number;
-}): Promise<Response<GetMenuReviewsData[]>> => {
+export const getMenuReviews = async (
+  menuId: number,
+): Promise<Response<GetMenuReviewsData[]>> => {
   const response = await apiInstance.get(`reviews/menus/${menuId}`);
   return response.data;
 };
@@ -37,8 +35,8 @@ export const getMenuReviews = async ({
 // * GET 특정 메뉴를 판매하는 식당 목록 응답
 export const getRestList = async (
   menuId: number,
-  page: number,
-): Promise<GetRestListData[]> => {
+  page?: number,
+): Promise<Response<GetRestListData[]>> => {
   const response = await apiInstance.get(
     `restaurants/menus/${menuId}?page=${page}`,
   );
@@ -48,14 +46,10 @@ export const getRestList = async (
 // * GET 최신 리뷰 리스트 응답
 export const getReviewList = async ({
   buildingId,
-  page,
 }: {
   buildingId: number;
-  page: number;
 }): Promise<Response<GetMenuReviewsData[]>> => {
-  const response = await apiInstance.get(
-    `reviews/buildings/${buildingId}?page=${page}`,
-  );
+  const response = await apiInstance.get(`reviews/buildings/${buildingId}`);
   return response.data;
 };
 
