5cad3160b276c1fb357ebe2ff379e1bc410d8247
junehee <dev.junehee@gmail.com>
2023-10-03 00:16:49 +0900

    feat: 기존 parseDate 함수 주석처리, 30분 더한 값 파싱 함수 재구현

--- src/utils/parseDate.ts
+++ src/utils/parseDate.ts
@@ -1,13 +1,32 @@
 // 서버로부터 내려오는 Date Data type이 ISO 8601 Date 형식일 때 사용되는 유틸 함수입니다.
-const parseDate = (dateProps: string) => {
-  // split을 통해 T 이후의 문자열 추출 ('2023-09-08T09:00:00Z') => ('09:00:00Z')
-  const [, time] = dateProps.split('T');
 
-  // split을 통해 : 이전, 이후의 문자열 추출 ('09:00:00Z') => ('09', '00')
-  const [hour, minute] = time.split(':');
+// 기존 함수
+// const parseDate = (dateProps: string) => {
+//   // split을 통해 T 이후의 문자열 추출 ('2023-09-08T09:00:00Z') => ('09:00:00Z')
+//   const [, time] = dateProps.split('T');
 
-  // '09:00'
-  return `${hour}:${minute}`;
+//   // split을 통해 : 이전, 이후의 문자열 추출 ('09:00:00Z') => ('09', '00')
+//   const [hour, minute] = time.split(':');
+
+//   // '09:00'
+//   return `${hour}:${minute}`;
+// };
+
+// 선택한 값에 30분을 더한 시간 출력
+const parseDate = (startTime: string) => {
+  const [hour, minute] = startTime.split(':');
+
+  const hourInt = parseInt(hour, 10);
+  const minuteInt = parseInt(minute, 10);
+
+  const newMinute = (minuteInt + 30) % 60;
+  const newHour = (hourInt + Math.floor((minuteInt + 30) / 60)) % 24;
+
+  const endTime = `${newHour.toString().padStart(2, '0')}:${newMinute
+    .toString()
+    .padStart(2, '0')}`;
+
+  return endTime;
 };
 
 export default parseDate;
