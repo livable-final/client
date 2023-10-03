a9ef00de7749e9244acc9a05d93d170438215ea5
Kled <1017yu@daum.net>
2023-10-03 11:33:14 +0900

    chore: remote domain 추가

--- next.config.js
+++ next.config.js
@@ -20,6 +20,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img1.kakaocdn.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   webpack: (config) => {
