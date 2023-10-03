6a5bcd427eee9a79f4ad6fb979c67262979e5ffe
Kled <1017yu@daum.net>
2023-10-03 11:35:11 +0900

    feat: 오늘 점심 지난 주 메뉴 랭킹 호출

--- src/components/lunch/ranking/index.tsx
+++ src/components/lunch/ranking/index.tsx
@@ -5,21 +5,21 @@ import usePagesStore from '@/stores/usePagesStore';
 import theme from '@/styles/theme';
 import { LUNCH_MAIN_CONSTANTS } from '@/constants/lunch';
 import { Right } from '@/assets/icons';
-// import useFetch from '@/hooks/useFetch';
-// import { getRanking } from '@/pages/api/lunch/lunchRequests';
-// import useBuildingStore from '@/stores/useBuildingStore';
-import { DUMMMY_MENU_TOP_TEN } from '@/constants/lunch/dummy';
+import useFetch from '@/hooks/useFetch';
+import { getRanking } from '@/pages/api/lunch/lunchRequests';
+import useUserStore from '@/stores/useUserStore';
 import COMPONENT_NAME from '@/constants/common/pages';
 
 // '오늘 점심' 홈의 랭킹 파트
 function LunchRanking() {
   const { title, heights, colors, margin } = LUNCH_MAIN_CONSTANTS.main.ranking;
   const { setNextComponent } = usePagesStore();
-  // const { buildingId } = useBuildingStore();
-  // const { response } = useFetch({
-  //   fetchFn: () => getRanking(buildingId),
-  // });
-  const top3Menus = DUMMMY_MENU_TOP_TEN?.slice(0, 3);
+  const { buildingId } = useUserStore();
+  const { response } = useFetch({
+    fetchFn: () => getRanking(buildingId),
+  });
+  const top3Menus = response?.data.slice(0, 3);
+  let sortedMenus;
 
   const onClickHandler = () => {
     setNextComponent(COMPONENT_NAME.lunch.detail.reviewByRanking); // 리뷰별 랭킹페이지로 이동
@@ -27,26 +27,29 @@ function LunchRanking() {
   };
 
   // 2위 - 1위 - 3위 순으로 정렬.
-  const sortedMenus = [
-    {
-      ...top3Menus[1],
-      height: heights[0],
-      color: colors[0],
-      margin: margin[0],
-    },
-    {
-      ...top3Menus[0],
-      height: heights[1],
-      color: colors[1],
-      margin: margin[1],
-    },
-    {
-      ...top3Menus[2],
-      height: heights[2],
-      color: colors[2],
-      margin: margin[2],
-    },
-  ];
+
+  if (response) {
+    sortedMenus = [
+      {
+        ...top3Menus[1],
+        height: heights[0],
+        color: colors[0],
+        margin: margin[0],
+      },
+      {
+        ...top3Menus[0],
+        height: heights[1],
+        color: colors[1],
+        margin: margin[1],
+      },
+      {
+        ...top3Menus[2],
+        height: heights[2],
+        color: colors[2],
+        margin: margin[2],
+      },
+    ];
+  }
 
   return (
     <LunchCard col nopad>
@@ -56,7 +59,7 @@ function LunchRanking() {
           <Right />
         </div>
         <div css={wrapperStyles}>
-          {sortedMenus.map((item) => (
+          {sortedMenus?.map((item) => (
             <LunchRankingPodium
               key={item.menuName}
               menuImage={item.menuImage}
