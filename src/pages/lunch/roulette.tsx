22ddd057f35c6064463fce9445ef80d9b0f1b120
Kled <1017yu@daum.net>
2023-10-03 11:44:33 +0900

    feat: 룰렛 가동 전 랜덤한 메뉴 기반 식당 추천 기능 추가

--- src/pages/lunch/roulette.tsx
+++ src/pages/lunch/roulette.tsx
@@ -4,18 +4,22 @@ import LunchReviewsByRest from '@/components/lunch/review/LunchReviewsByRest';
 import LunchRoulettePopup from '@/components/lunch/roulette/LunchRoulettePopup';
 import { LUNCH_ROULETTE_CONSTANTS } from '@/constants/lunch';
 import useRouletteStore from '@/stores/useRouletteStore';
+import getRandomNumber from '@/utils/getRandomNumber';
 import { css } from '@emotion/react';
 
 function Roulette() {
   const { isOperated, isAgain, isSelected } = useRouletteStore();
-  const { title } = LUNCH_ROULETTE_CONSTANTS;
+  const { title, menu } = LUNCH_ROULETTE_CONSTANTS;
+  const randomMenuId = getRandomNumber(menu.max);
+
   return (
     <>
       <Header title={title.roulette} isSticky />
       <section css={sectionStyle}>
         <LunchRoulette />
         <LunchRoulettePopup />
-        {isOperated && isAgain && isSelected && <LunchReviewsByRest />}
+        {(isOperated && isAgain && isSelected && <LunchReviewsByRest />) ||
+          (!isAgain && <LunchReviewsByRest menuId={randomMenuId} />)}
       </section>
     </>
   );
@@ -24,6 +28,7 @@ function Roulette() {
 const sectionStyle = css`
   display: flex;
   flex-direction: column;
+  gap: 16px;
 `;
 
 export default Roulette;
