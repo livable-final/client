d75f0008d914688c7c8472e3e68f8176d8a1beb6
Siwoo Lee <siwoo28@kakao.com>
2023-10-04 11:03:46 +0900

    fix: 초대 목록 QA 반영

--- src/components/user/UserInvitationListItem.tsx
+++ src/components/user/UserInvitationListItem.tsx
@@ -2,6 +2,7 @@ import theme from '@/styles/theme';
 import changeVisitPurpose from '@/utils/changeVisitPurpose';
 import { css } from '@emotion/react';
 import { UserInvitationListItemProps } from '@/types/user/invitationList';
+import mq from '@/utils/mediaquery';
 
 function UserInvitationListItem({ data }: UserInvitationListItemProps) {
   const visitorCount = data.visitorCount - 1;
@@ -22,7 +23,8 @@ function UserInvitationListItem({ data }: UserInvitationListItemProps) {
         <div css={placeStyles}>{data.officeName}</div>
       </div>
       <div css={timeStyles}>
-        {startTime}~{endTime}
+        <div>{startTime}~</div>
+        <div>{endTime}</div>
       </div>
     </div>
   );
@@ -47,9 +49,10 @@ const firstLineStyles = css`
   display: flex;
   flex-direction: row;
   align-items: center;
+  margin-bottom: 2px;
 `;
 const visitorInfoStyles = css`
-  font: ${theme.font.subTitle.subTitle1_400};
+  font: ${theme.font.subTitle.subTitle2_400};
   color: ${theme.palette.title};
   margin-right: 8px;
 `;
@@ -59,10 +62,20 @@ const purposeStyles = css`
 `;
 const placeStyles = css`
   font: ${theme.font.body.body2_400};
-  color: ${theme.palette.greyscale.grey40};
+  color: ${theme.palette.greyscale.grey50};
+  text-align: left;
 `;
 const timeStyles = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: right;
   font: ${theme.font.body.body1_400};
   color: ${theme.palette.primary};
+  ${mq.md} {
+    display: flex;
+    flex-direction: row;
+  }
+  ${mq.tab} {
+  }
 `;
 export default UserInvitationListItem;
