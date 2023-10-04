ff0c31c9372f4bbccd102c61837e545bf2ca845a
ChoEun-Sang <wh94es@gmail.com>
2023-10-05 00:14:35 +0900

    refactor: 리뷰가 한 개일 때, 닫기 오류 해결

--- src/components/lunch/calendar/LunchCalendarDetailsSlide.tsx
+++ src/components/lunch/calendar/LunchCalendarDetailsSlide.tsx
@@ -29,10 +29,15 @@ function LunchCalendarDetailsSlide() {
       | React.KeyboardEvent<HTMLDivElement>,
   ) => {
     event.preventDefault();
+    console.log(event);
 
     const target = event.target as HTMLInputElement;
     // click 이벤트 발생할 때, e.target과 e.currentTarget이 달라서 비교 조건 사용 불가
-    if (target.className === 'swiper-slide swiper-slide-active') setIsChecked();
+    if (
+      target.className === 'swiper-slide swiper-slide-active' ||
+      target.className === 'swiper-slide swiper-slide-active swiper-slide-next'
+    )
+      setIsChecked();
   };
 
   return (
