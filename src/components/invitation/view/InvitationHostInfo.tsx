f3c6b5550cc60e397a67bf7413bbccfa96b5ebcf
Siwoo Lee <siwoo28@kakao.com>
2023-10-04 19:07:50 +0900

    fix: 초대자 정보 페이지QA 반영

--- src/components/invitation/view/InvitationHostInfo.tsx
+++ src/components/invitation/view/InvitationHostInfo.tsx
@@ -2,7 +2,7 @@ import { css } from '@emotion/react';
 import theme from '@/styles/theme';
 import Header from '@/components/common/Header';
 import InvitationHostInfoItem from '@/components/invitation/view/InvitationHostInfoItem';
-import { Profile } from '@/assets/icons';
+import Icon from '@/components/common/Icons';
 import { InvitationInfoContainerProps } from '@/types/invitation/view';
 import { INVITATION_VEIW_INFO_TEXTS } from '@/constants/invitation/viewTexts';
 
@@ -11,7 +11,7 @@ function InvitationHostInfo({ data }: InvitationInfoContainerProps) {
     <div css={InvitationHostInfoStyles}>
       <Header title={INVITATION_VEIW_INFO_TEXTS.category.host} isBg />
       <div css={InvitationHostInfoProfileStyles}>
-        <Profile />
+        <Icon icon="reviewer" size="70" />
       </div>
       <ul css={InvitationInfoItemContinerStyles}>
         <InvitationHostInfoItem
@@ -28,8 +28,10 @@ function InvitationHostInfo({ data }: InvitationInfoContainerProps) {
           isContact
         />
       </ul>
-      <div css={InvitationHostInfoCardSyles}>
-        <div>{INVITATION_VEIW_INFO_TEXTS.host.card}</div>
+      <div css={InvitationHostInfoCardContainerSyles}>
+        <div css={InvitationHostInfoCardSyles}>
+          <div>{INVITATION_VEIW_INFO_TEXTS.host.card}</div>
+        </div>
       </div>
       <div css={InvitationHostInfoBackGroundStyles} />
     </div>
@@ -39,6 +41,7 @@ const InvitationHostInfoStyles = css`
   display: flex;
   flex-direction: column;
   position: relative;
+  justify-content: space-between;
 `;
 const InvitationHostInfoBackGroundStyles = css`
   position: absolute;
@@ -48,33 +51,31 @@ const InvitationHostInfoBackGroundStyles = css`
   max-width: 1024px;
   height: 258px;
   background-clip: border-box;
-  background-image: linear-gradient(
-    -200deg,
-    rgba(97, 132, 255, 0.3) 1.42%,
-    rgba(97, 132, 255, 0.16) 27.47%,
-    rgba(97, 132, 255, 0.09) 57.59%,
-    rgba(97, 132, 255, 0) 94.71%
-  );
+  background-color: ${theme.palette.bluescale.blue10};
 `;
 const InvitationHostInfoProfileStyles = css`
   display: flex;
   justify-content: center;
   align-items: center;
   color: ${theme.palette.greyscale.grey30};
-  background-color: ${theme.palette.white};
-  width: 108px;
-  height: 108px;
+  background-color: ${theme.palette.bluescale.blue20};
+  width: 100px;
+  height: 100px;
   margin: 44px auto;
   border-radius: 50%;
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.1);
 `;
 const InvitationInfoItemContinerStyles = css`
+  font: ${theme.font.subTitle.subTitle1_500};
   margin-top: 30px;
 `;
+const InvitationHostInfoCardContainerSyles = css`
+  overflow-x: scroll;
+`;
 const InvitationHostInfoCardSyles = css`
-  min-width: 350px;
-  min-height: 158px;
-  margin: 62px 4px 38px;
+  width: 358px;
+  height: 200px;
+  margin: 62px auto 38px;
   display: flex;
   justify-content: center;
   border-radius: 16px;
