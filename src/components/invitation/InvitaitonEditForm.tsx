0c44b7a37acce92f6397e22910add49ef989da9e
Siwoo Lee <siwoo28@kakao.com>
2023-10-05 12:48:47 +0900

    fix: 초대장 다시 보내기 페이지 레이아웃 수정

--- src/components/invitation/InvitaitonEditForm.tsx
+++ src/components/invitation/InvitaitonEditForm.tsx
@@ -2,14 +2,38 @@ import usePagesStore from '@/stores/usePagesStore';
 import InvitationDoneContainer from '@/components/invitation/create/InvitationDoneContainer';
 import InvitationEdit from '@/components/invitation/edit/InvitationEdit';
 import { InvitationEditProps } from '@/types/invitation/edit';
+import { css } from '@emotion/react';
+import mq from '@/utils/mediaquery';
 
 function InvitaitonEditForm({ id }: InvitationEditProps) {
   const { nextComponent } = usePagesStore();
   if (nextComponent === 'InvitationDoneContainer') {
-    return <InvitationDoneContainer />;
+    return (
+      <div css={containerStyles}>
+        <InvitationDoneContainer />;
+      </div>
+    );
   }
 
   return <InvitationEdit id={id} />;
 }
 
+const containerStyles = css`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  ${mq.md} {
+    max-width: 360px;
+  }
+  ${mq.lg} {
+    max-width: 480px;
+  }
+  ${mq.tab} {
+    max-width: 640px;
+  }
+`;
 export default InvitaitonEditForm;
